1b0f8af8a355eaaeabc6b5bf357731fc790ad89e
rjf2 <robert.fasano@nist.gov>
2018-03-08 11:25:15 -0700

    Added options for decimal rounding and number of digits per column in formatRow()

--- IO.py
+++ IO.py
@@ -2,11 +2,16 @@ def cleanRow(row):
     ''' Removes whitespace from row to enforce tab-delimitation only '''
     return formatRow(parseRow(row))
 
-def formatRow(lst, newline = 'end'):
-    ''' Converts a list of values into a tab-delimited string for file-output '''
+def formatRow(lst, newline = 'end', decs = [], lengths = []):
+    ''' Converts a list of values into a tab-delimited string for file-output 
+        If custom column output lengths are desired, pass a number into dec for each number of decimal places
+        and into length for each total length'''
     row = ''
-    for val in lst:
-        row += str(val) + '\t'
+    for i in range(len(lst)):
+        if len(decs) == 0:
+            row += str(lst[i]) + '\t'
+        else:
+            row += ('{0:0.{prec}f}'.format(lst[i], prec=decs[i])).ljust(lengths[i], '0')+'\t'
     if newline == 'end':
         row = row[0:-1] + '\n'
     elif newline == 'start':
@@ -22,3 +27,4 @@ def parseRow(row):
         if val != '\n':
             lst.append(val.strip())    
     return lst
+
