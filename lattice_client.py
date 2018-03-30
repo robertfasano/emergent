805c0b3f6e872de727a45f5a422ef19507f1f885
= <robert.fasano@nist.gov>
2018-03-30 16:42:10 -0600

    Some lock parameters are now passed remotely when the lock command is issued.

--- lattice_client.py
+++ lattice_client.py
@@ -7,31 +7,41 @@ class Client():
     def __init__(self):
         # Create a TCP/IP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+        # connect to mcdaq
+#        params = {'device':'USB-2416', 'id':'1C2678C'}
+        params = {'device':'USB-2416', 'id':'1C26788'}
 
-        # Connect the socket to the port where the server is listening
+        self.daq = MCDAQ(params, function = 'input')
+        
+    def connect(self):
         server_address = ('132.163.82.22', 6666)
         print('Connecting to %s port %s' % server_address)
         self.sock.connect(server_address)
-    
-        # connect to mcdaq
-        params = {'device':'USB-2416', 'id':'1C2678C'}
-#        self.daq = MCDAQ(params, function = 'input')
         
     def message(self, op, parameters = {}):
         ''' Sends a command to the server '''
         cmd = { "message": {"transmission_id": [1001],"op": op,"parameters": parameters }}
         self.sock.sendall(json.dumps(cmd).encode())
+        if op != 'data':
+            print('Sent: ', cmd)
         return self.receive()
         
     def ping(self):
         reply = self.message(op='ping', parameters={'time': astropy.time.Time.now().unix})
         print(reply)
         
+    def acquire_etalon_lock(self):
+        self.message(op='acquire_etalon_lock')  
+        
     def set_cavity(self, value):
         self.message(op='set_cavity', parameters = value)
         
     def receive(self):
-        return json.loads(self.sock.recv(1024).decode())
+        reply = json.loads(self.sock.recv(1024).decode())
+        if reply['message']['op'] != 'request':
+            print('Received: ', reply['message']['parameters'])
+        return reply
     
     def tune_etalon(self):
         reply = self.message(op='tune_etalon')
@@ -41,40 +51,29 @@ class Client():
         reply = self.message(op='tune_cavity')
         print(reply)
         
-    def lock(self):
+    def lock(self, parameters):
         try:
-#            cmd = { "message": {
-#                    "transmission_id": [1001],
-#                    "op": "lock",
-#                    "parameters": {}}
-#                  }
-            # Send data
-#            message = b'%f'%V
-#            print('sending "%s"' % message)
-            self.message(op='lock')
+            self.message(op='lock', parameters = parameters)
             
+            ''' Begin listening mode for status updates during the etalon lock process '''
             while True:
-#                reply = json.loads(self.sock.recv(1024).decode())
                 reply = self.receive()
-#                print(reply)
-#                if reply == '':
-#                    break
-                print(reply['message']['msg'])
                 if reply['message']['op'] == 'done':
                     break
             
-            ''' Now start streaming data from the ADC '''
+            ''' When the etalon lock process is done, the server will send a 'done' message. Now start streaming data from the ADC. This is done by first requesting status from the server and responding with a voltage if needed. '''
+            reply = self.receive()
             while True:
-                print('reading from ADC')
-                V = self.daq.read(3)
-                self.message(op='data', parameters = {'V':V})
-                reply = self.receive()
+                if reply['message']['op'] == 'request':
+#                    V = self.daq.read(3)
+                    transmission = self.daq.read(5)
+                    output = self.daq.read(15)
+                    reply = self.message(op='data', parameters = {'transmission':transmission, 'output':output})
 
-                print(reply['message']['msg'])
                 if reply['message']['op'] == 'done':
                     break
-        finally:
-            self.sock.close()
+        except KeyboardInterrupt:
+            print('Locking routine aborted by user.')
          
     def send(self, cmd):
         cmd = json.dumps(cmd).encode('ascii')
@@ -82,9 +81,11 @@ class Client():
 
 if __name__ == '__main__':
     try:
-        c
+        c.connect()
     except NameError:    
         c = Client()
-    c.ping()
-#    c.lock()
+#    c.ping()
+    parameters = {'pzt_center_gain':.0002, 'cavity_tune_delay':.5, 'sweep_steps':60, 'cavity_tune_threshold':.02, 'cavity_transmission_threshold':0.1, 'pzt_center_threshold':.3}
+    c.lock(parameters)
+    
             
