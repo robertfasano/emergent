c5a081f6c6c71ce815c8cb673e482708d0b819e2
= <robert.fasano@nist.gov>
2018-05-07 14:03:08 -0600

    Universal serial communications methods

---
+++ protocols/serial.py
@@ -0,0 +1,36 @@
+import serial
+
+class Serial():
+    def __init__(self, port = 'COM1', baudrate = 19200, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1, encoding = 'ascii'):
+        self.port, self.baudrate, self.parity, self.stopbits, self.bytesize, self.timeout, self.encoding = port, baudrate, parity, stopbits, bytesize, timeout, encoding
+        self.connect()
+        
+    def connect(self):
+        self.ser = serial.Serial(
+                port=self.port,
+                baudrate=self.baudrate,
+                parity=self.parity,
+                stopbits=self.stopbits,
+                bytesize=self.bytesize,
+                timeout = self.timeout
+            )
+        
+        if self.ser.isOpen():
+            self.ser.close()
+#        if self.ser.isOpen() != 1:
+        self.ser.open()
+            
+    def command(self, cmd):
+        cmd += '\r'
+        self.ser.write(cmd.encode(self.encoding))
+        reply = self.ser.readline()
+        
+        return reply.decode()
+#        if query == True:
+#            reply = reply.decode()
+#            return reply
+    
+
+        
+
+
