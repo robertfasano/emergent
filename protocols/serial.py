e9f2abfad75bfc623e0d7f9a56b8d9694a57a383
twodimensionalanimal <34033655+twodimensionalanimal@users.noreply.github.com>
2018-06-24 13:57:19 -0600

    Added _connected variable which is set to 1 on successful connection

--- protocols/serial.py
+++ protocols/serial.py
@@ -1,24 +1,33 @@
 import serial
 
+PARITY_NONE = serial.PARITY_NONE 
+STOPBITS_ONE = serial.STOPBITS_ONE
+EIGHTBITS = serial.EIGHTBITS
+
 class Serial():
     def __init__(self, port = 'COM1', baudrate = 19200, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1, encoding = 'ascii'):
         self.port, self.baudrate, self.parity, self.stopbits, self.bytesize, self.timeout, self.encoding = port, baudrate, parity, stopbits, bytesize, timeout, encoding
+        self._connected = 0
         self.connect()
         
     def connect(self):
-        self.ser = serial.Serial(
-                port=self.port,
-                baudrate=self.baudrate,
-                parity=self.parity,
-                stopbits=self.stopbits,
-                bytesize=self.bytesize,
-                timeout = self.timeout
-            )
-        
-        if self.ser.isOpen():
-            self.ser.close()
-#        if self.ser.isOpen() != 1:
-        self.ser.open()
+        try:
+            self.ser = serial.Serial(
+                    port=self.port,
+                    baudrate=self.baudrate,
+                    parity=self.parity,
+                    stopbits=self.stopbits,
+                    bytesize=self.bytesize,
+                    timeout = self.timeout
+                )
+            
+            if self.ser.isOpen():
+                self.ser.close()
+    #        if self.ser.isOpen() != 1:
+            self.ser.open()
+            self._connected = 1
+        except serial.serialutil.SerialException:
+            print('Could not open port %s'%self.port)
             
     def command(self, cmd, output = False, reply = True, suffix = '\r'):
         cmd += suffix
