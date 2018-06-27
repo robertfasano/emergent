b8b7c46a931ab760f8c93072f808723600d41254
= <robert.fasano@nist.gov>
2018-06-27 17:36:43 -0600

    Added stoppable Thread class

--- archetypes/Parallel.py
+++ archetypes/Parallel.py
@@ -1,10 +1,10 @@
 import multiprocessing
-
+import threading
 class ProcessHandler():
     def __init__(self):
         self.threads = []
         
-    def run_in_process(self, target, args = None):
+    def run_process(self, target, args = None):
         ''' Allows a target function of the parent object to be run in a process '''
         if type(target) == str:
             target = getattr(self, target)
@@ -19,6 +19,21 @@ class ProcessHandler():
             if thread.target == target:
                 thread.stop()
                 
+    def run_thread(self, target, args = None):
+        ''' Allows a target function of the parent object to be run in a thread '''
+        if type(target) == str:
+            target = getattr(self, target)
+        assert callable(target)
+        Thread(target=target, args=args, parent = self)            
+        
+    def quit_thread(self, target, args = None):
+        if type(target) == str:
+            target = getattr(self, target)
+        assert callable(target)
+        for thread in self.threads:
+            if thread.target == target:
+                thread.stop()   
+    
 class Process(multiprocessing.Process):
     def __init__(self, target, args, parent):
         self.target = target
@@ -40,8 +55,41 @@ class Process(multiprocessing.Process):
         if self in self.parent.threads:
             del self.parent.threads[self.parent.threads.index(self)]
         self.terminate()
-        
 
+class Thread(threading.Thread):
+    def __init__(self, target, args, parent):
+        self.target = target
+        self._stopper = threading.Event()
+
+        if args is not None:
+            args = list(args)
+            args.append(self.stopped)
+            args = tuple(args)
+            super().__init__(target=target, args = args)
+        else: 
+            args = (self.stopped)
+            super().__init__(target=target, args = args)
+            
+        ''' Add process to parent '''
+        self.parent = parent
+        if not hasattr(self.parent, 'threads'):
+            self.parent.threads = [self]
+        else:
+            self.parent.threads.append(self)
+            
+        self.start()
+        
+    def stop(self):
+        if self in self.parent.threads:
+            del self.parent.threads[self.parent.threads.index(self)]
+        self._stopper.set()
+        
+    def stopped(self):
+        return self._stopper.is_set()
+    
+
+        
+    
 class test_class(ProcessHandler):
     def __init__(self):
         super().__init__()
