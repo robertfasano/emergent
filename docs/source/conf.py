67616a2862e7ef64ffb49ece3f4a4e4dd6b22ea3
Robert Fasano <robert.j.fasano@colorado.edu>
2018-07-17 22:16:06 -0600

    Fix typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'pandas', PyQt5]
+MOCK_MODULES = ['numpy', 'pandas', 'PyQt5']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
