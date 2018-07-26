1e691c45f3adc9990f6908fa7ab0783555869cf2
Robert Fasano <robert.j.fasano@colorado.edu>
2018-07-26 18:03:42 +0200

    Rename labAPI->EMERGENT

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -129,7 +129,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'labAPI.tex', 'labAPI Documentation',
+    (master_doc, 'EMERGENT.tex', 'EMERGENT Documentation',
      'Robert Fasano', 'manual'),
 ]
 
@@ -150,7 +150,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'labAPI', 'labAPI Documentation',
-     author, 'labAPI', 'One line description of project.',
+    (master_doc, 'EMERGENT', 'EMERGENT Documentation',
+     author, 'EMERGENT', 'One line description of project.',
      'Miscellaneous'),
 ]
