7c1b18c67e4b67e6b205de271d6e9f0c5b766f0f
Robert Fasano <robert.j.fasano@colorado.edu>
2018-07-27 22:41:41 -0600

    Fix relative import path

--- examples/basic/main.py
+++ examples/basic/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 char = {'nt': '\\', 'posix': '/'}[os.name]
-sys.path.append(char.join(os.getcwd().split(char)[0:-1]))
+sys.path.append(char.join(os.getcwd().split(char)[0:-3]))
 from emergent.archetypes.node import Control
 from emergent.devices.test import TestDevice
 import numpy as np
@@ -9,7 +9,7 @@ import numpy as np
 class TestControl(Control):
         def __init__(self, name, parent=None):
                 super().__init__(name, parent)
-                
+
         def cost(state):
                 self.actuate(state)
                 x0 = .3
