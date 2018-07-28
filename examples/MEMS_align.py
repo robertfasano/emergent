12cd06a18cbf7d28f8a5a7e7222f532395c9775b
Robert Fasano <robert.j.fasano@colorado.edu>
2018-07-28 05:22:12 +0200

    Initial commit

---
+++ examples/MEMS_align.py
@@ -0,0 +1,26 @@
+import sys
+import os
+char = {'nt': '\\', 'posix': '/'}[os.name]
+sys.path.append(char.join(os.getcwd().split(char)[0:-1]))
+from emergent.archetypes.node import Control
+from emergent.devices.picoAmp import PicoAmp
+from emergent.devices.labjackT7 import LabJack
+import numpy as np
+
+class AutoAlign(Control):
+        def __init__(name, labjack, parent=None):
+                super().__init__(name, parent)
+                self.labjack = LabJack()
+                
+        def cost(state):
+                self.actuate(state)
+                return self.labjack.AIn(0)
+
+devid = '160049734'
+labjack = LabJack(devid=devid)
+control = Control('control')
+
+mems = PicoAmp('MEMS', labjack, parent=control)
+mems.add_input('X', 0, 0, 1)
+mems.add_input('Y', 0, 0, 1)
+
