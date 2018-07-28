b92b65e140321c4e27688271cf560157c133a275
Robert Fasano <robert.j.fasano@colorado.edu>
2018-07-27 21:05:29 -0600

    Fix import typo

--- examples/basic.py
+++ examples/basic.py
@@ -3,7 +3,7 @@ import os
 char = {'nt': '\\', 'posix': '/'}[os.name]
 sys.path.append(char.join(os.getcwd().split(char)[0:-1]))
 from emergent.archetypes.node import Control
-from emergent.devices import TestDevice
+from emergent.devices.test import TestDevice
 import numpy as np
 
 def gaussian(state):
@@ -12,7 +12,7 @@ def gaussian(state):
         for x in state.values():
                 cost *= np.exp(-x**2/x0**2)
         return cost
-        
+
 control = Control('control', cost = gaussian)
 
 deviceA = TestDevice('deviceA', parent=control)
@@ -21,4 +21,3 @@ deviceA.add_input('Y', 0, 0, 1)
 
 deviceB = TestDevice('deviceB', parent=control)
 deviceB.add_input('Z', 0, 0, 1)
-
