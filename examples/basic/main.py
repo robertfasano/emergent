6b657f27c88a60992fb8256813775a2cd9cc8c6e
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-01 00:36:09 +0200

    Add optimizer to control node

--- examples/basic/main.py
+++ examples/basic/main.py
@@ -4,12 +4,15 @@ char = {'nt': '\\', 'posix': '/'}[os.name]
 sys.path.append(char.join(os.getcwd().split(char)[0:-3]))
 from emergent.archetypes.node import Control
 from emergent.devices.test import TestDevice
+from emergent.archetypes.Optimizer import Optimizer
+
 import numpy as np
 
 class TestControl(Control):
         def __init__(self, name, parent=None):
                 super().__init__(name, parent)
-
+                self.optimizer = Optimizer(self)
+                
         def cost(state):
                 self.actuate(state)
                 x0 = .3
@@ -26,3 +29,5 @@ deviceA.add_input('Y', 0, 0, 1)
 
 deviceB = TestDevice('deviceB', parent=control)
 deviceB.add_input('Z', 0, 0, 1)
+
+control.optimizer.optimize(state=control.state, cost=control.cost, method='grid_search)
