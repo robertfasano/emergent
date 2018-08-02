8e4a7a1b31ae796ed65eba00f8cc1b9622963321
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-02 04:25:26 -0600

    Add ProcessHandler and fix bug with delays

--- archetypes/Clock.py
+++ archetypes/Clock.py
@@ -1,10 +1,12 @@
 import time
 import numpy as np
 from threading import Thread
+from emergent.archetypes.Parallel import ProcessHandler
 
-class Clock():
+class Clock(ProcessHandler):
     def __init__(self, parent):
         ''' Initializes the Clock and attaches it to a parent Control node '''
+        ProcessHandler.__init__(self)
         self.parent = parent
         self.inputs = []
         self.state = {}
@@ -14,25 +16,26 @@ class Clock():
 
     def start(self, T):
         states = self.prepare_sequence(T)
-        self.loop_thread = Thread(target=self.loop)
-        self.loop_thread.start()
+        self._run_thread(self.loop)
+        self._run_thread(self.sync)
 
-        self.sync_thread = Thread(target=self.sync)
-        self.sync_thread.start()
+    def stop(self):
+        self._quit_thread(self.loop)
+        self._quit_thread(self.sync)
 
-    def loop(self):
+    def loop(self, stopped):
         ''' Loops through the specified sequence. The argument states is a list of tuples where the first element of each tuple is a delay and the second is a target state.'''
         i = 0
-        while True:
+        while not stopped():
             delay = self.sequence[i][0]
             state = self.sequence[i][1]
             time.sleep(delay)
             self.state = state
             i = (i+1)%len(self.sequence)
 
-    def sync(self):
+    def sync(self, stopped):
         ''' Keeps the physical state synced with another internal variable which can be set from another thread. This is useful for running sequences without additional delay due to actuation. '''
-        while True:
+        while not stopped():
             parent_substate = dict((k, self.parent.state[k]) for k in self.state.keys())
             if self.state != parent_substate:
                 self.parent.actuate(self.state)
@@ -43,7 +46,8 @@ class Clock():
         for input in self.inputs:
             times.extend([x[0]*T for x in input.sequence])
         times = np.unique(times)
-        delays = np.append(np.array([0]),np.diff(times))
+
+        delays = np.append(np.array([T-np.sum(np.diff(times))]),np.diff(times))
         states = []
         for i in range(len(times)):
             t = times[i]
