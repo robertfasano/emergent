d2cb776359f6d8494c0621537d0a0b5f1a7df82a
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-02 20:39:06 -0600

    Remove unused code and reorder remaining

--- gui/elements/treeview.py
+++ gui/elements/treeview.py
@@ -8,9 +8,8 @@ from PyQt5.QtWidgets import (QApplication, QAbstractItemView,QCheckBox, QComboBo
         QGroupBox, QHBoxLayout, QLabel, QTextEdit, QTreeView, QPushButton, QTableView,QVBoxLayout,
         QWidget, QMenu, QAction, QTreeWidget, QTreeWidgetItem)
 from PyQt5.QtCore import *
-from emergent.gui.elements.Optimizer import OptimizerWindow
-from emergent.archetypes.Optimizer import Optimizer
 import json
+from emergent.archetypes.Optimizer import Optimizer
 
 class MainFrame(QWidget):
     def __init__(self, tree, control):
@@ -22,11 +21,12 @@ class MainFrame(QWidget):
         self.control = control
         self.control.window = self
         self.tree = tree
-        self.optimizerWindow = OptimizerWindow(self.control)
 
         self.currentItem = None
         self.lastItem = None
+        layout= QHBoxLayout(self)
 
+        ''' Create QTreeWidget '''
         self.treeWidget = QTreeWidget()
         self.treeWidget.setSelectionMode(QAbstractItemView.MultiSelection)
         self.treeWidget.setColumnCount(2)
@@ -35,21 +35,24 @@ class MainFrame(QWidget):
         self.treeWidget.itemDoubleClicked.connect(self.open_editor)
         self.treeWidget.currentItemChanged.connect(self.close_editor)
         self.treeWidget.itemChanged.connect(self.close_editor)
-
         self.treeWidget.setContextMenuPolicy(Qt.CustomContextMenu)
         self.treeWidget.customContextMenuRequested.connect(self.openMenu)
+        treeLayout = QHBoxLayout()
+        treeLayout.addWidget(self.treeWidget)
+        layout.addLayout(treeLayout)
 
+        ''' Populate QTreeWidget '''
         root_label = list(self.tree.keys())[0]
         root = QTreeWidgetItem([root_label, ''])
         self.treeWidget.insertTopLevelItems(0, [root])
         self._generateTree(self.tree[root_label], root)
+
+        ''' Prepare initial GUI state '''
         self.get_state()
         self.expand(0)
         self.expand(1)
 
-        treeLayout = QHBoxLayout()
-        treeLayout.addWidget(self.treeWidget)
-
+        ''' Create optimizer layout '''
         optimizerLayout = QVBoxLayout()
         optimizerLayout.addWidget(QLabel('Optimizer'))
         self.algorithm_box = QComboBox()
@@ -69,64 +72,15 @@ class MainFrame(QWidget):
         self.optimizer_button = QPushButton('Go!')
         self.optimizer_button.clicked.connect(self.start_optimizer)
         optimizerLayout.addWidget(self.optimizer_button)
+        layout.addLayout(optimizerLayout)
 
         ''' Ensure that only Inputs are selectable '''
         for item in self.get_all_items():
             if self.get_layer(item) != 2:
                 item.setFlags(Qt.ItemIsEnabled)
 
-
-        layout= QHBoxLayout(self)
-
-
-        layout.addLayout(treeLayout)
-        layout.addLayout(optimizerLayout)
-
-    def start_optimizer(self):
-        func = getattr(self.control.optimizer, self.algorithm_box.currentText())
-        params = self.params_edit.toPlainText().replace('\n','').replace("'", '"')
-        print(params)
-        params = json.loads(params)
-        cost = getattr(self.control, self.cost_box.currentText())
-        state = self.get_selected_state()
-        if state == {}:
-            print('Please select at least one Input node for optimization.')
-        else:
-            points, cost = func(state, cost, params)
-
-    def update_algorithm(self):
-        f = getattr(Optimizer, self.algorithm_box.currentText())
-        doc = inspect.getdoc(f)
-        # self.algorithm_label.setText(doc)
-        args = inspect.signature(f).parameters
-        args = list(args.items())
-        arguments = []
-        for a in args:
-            name = a[0]
-            default = str(a[1])
-            if default == name:
-                default = 'Enter'
-            else:
-                default = default.split('=')[1]
-                default = default.replace('{', '{\n')
-                default = default.replace(',', ',\n')
-                default = default.replace('}', '\n}')
-                self.params_edit.setText(default)
-
-    def expand(self, layer):
-        items = self.get_all_items()
-        items = [x for x in items if self.get_layer(x)==layer]
-        for item in items:
-            item.setExpanded(True)
-
-
-    def open_editor(self):
-        self.currentItem = self.treeWidget.currentItem()
-        col = self.treeWidget.currentIndex().column()
-        if col == 1:
-            self.treeWidget.openPersistentEditor(self.treeWidget.currentItem(), col)
-
     def close_editor(self):
+        ''' Send actuate command and disable editing after the user presses return or clicks another node. '''
         try:
             if self.lastItem != self.currentItem:
                 self.lastItem = self.currentItem
@@ -142,11 +96,32 @@ class MainFrame(QWidget):
         except AttributeError:
             pass
 
+    def expand(self, layer):
+        ''' Expand all nodes in a given layer. '''
+        items = self.get_all_items()
+        items = [x for x in items if self.get_layer(x)==layer]
+        for item in items:
+            item.setExpanded(True)
 
-    def get_device(self, name):
-        return self.treeWidget.findItems(name, Qt.MatchExactly|Qt.MatchRecursive, 0)[0]
+    def _generateTree(self, children, parent):
+        ''' Recursively add nodes to build the full network. '''
+        for child in sorted(children):
+            child_item = QTreeWidgetItem([child])
+            parent.addChild(child_item)
+
+            if isinstance(children, dict):
+                self._generateTree(children[child], child_item)
+
+    def get_all_items(self):
+        """Returns all QTreeWidgetItems in the given QTreeWidget."""
+        all_items = []
+        for i in range(self.treeWidget.topLevelItemCount()):
+            top_item = self.treeWidget.topLevelItem(i)
+            all_items.extend(self.get_subtree_nodes(top_item))
+        return all_items
 
     def get_input(self, full_name):
+        ''' Return input item corresponding to a full_name. '''
         device_name = full_name.split('.')[0]
         input_name = full_name.split('.')[1]
         inputs = self.treeWidget.findItems(input_name, Qt.MatchExactly|Qt.MatchRecursive, 0)
@@ -157,6 +132,15 @@ class MainFrame(QWidget):
     def get_items_on_level(self, level):
         all_items = self.get_all_items()
 
+    def get_layer(self, item):
+        ''' Get the layer in which an item resides. '''
+        layer = 0
+        parent = 0
+        while item is not None:
+            item = item.parent()
+            layer +=1
+        return layer-1
+
     def get_subtree_nodes(self, item):
         """Returns all QTreeWidgetItems in the subtree rooted at the given node."""
         nodes = []
@@ -165,48 +149,78 @@ class MainFrame(QWidget):
             nodes.extend(self.get_subtree_nodes(item.child(i)))
         return nodes
 
-    def get_all_items(self):
-        """Returns all QTreeWidgetItems in the given QTreeWidget."""
-        all_items = []
-        for i in range(self.treeWidget.topLevelItemCount()):
-            top_item = self.treeWidget.topLevelItem(i)
-            all_items.extend(self.get_subtree_nodes(top_item))
-        return all_items
-
     def get_state(self):
+        ''' Read Control node state and update GUI. '''
         for key in self.control.state:
             self.get_input(key).setText(1, '%.2f'%self.control.state[key])
 
-    def _generateTree(self, children, parent):
-        for child in sorted(children):
-            child_item = QTreeWidgetItem([child])
-            parent.addChild(child_item)
+    def open_editor(self):
+        ''' Allow the currently-selected node to be edited. '''
+        self.currentItem = self.treeWidget.currentItem()
+        col = self.treeWidget.currentIndex().column()
+        if col == 1:
+            self.treeWidget.openPersistentEditor(self.treeWidget.currentItem(), col)
 
-            if isinstance(children, dict):
-                self._generateTree(children[child], child_item)
+    def start_optimizer(self):
+        ''' Call chosen optimization routine with user-selected cost function and parameters '''
+        func = getattr(self.control.optimizer, self.algorithm_box.currentText())
+        params = self.params_edit.toPlainText().replace('\n','').replace("'", '"')
+        print(params)
+        params = json.loads(params)
+        cost = getattr(self.control, self.cost_box.currentText())
+        state = self.get_selected_state()
+        if state == {}:
+            print('Please select at least one Input node for optimization.')
+        else:
+            points, cost = func(state, cost, params)
 
-    def get_layer(self, item):
-        layer = 0
-        parent = 0
-        while item is not None:
-            item = item.parent()
-            layer +=1
-        return layer-1
+    def update_algorithm(self):
+        f = getattr(Optimizer, self.algorithm_box.currentText())
+        ''' Read default params dict from source code and insert in self.params_edit. '''
+        args = inspect.signature(f).parameters
+        args = list(args.items())
+        arguments = []
+        for a in args:
+            name = a[0]
+            default = str(a[1])
+            if default == name:
+                default = 'Enter'
+            else:
+                default = default.split('=')[1]
+                default = default.replace('{', '{\n')
+                default = default.replace(',', ',\n')
+                default = default.replace('}', '\n}')
+                self.params_edit.setText(default)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def openMenu(self, pos):
         level = self.get_selected_level()
         globalPos = self.mapToGlobal(pos)
         menu = QMenu()
 
-        if level == 0:
-            optimize_action = QAction('Optimize', self)
-            optimize_action.triggered.connect(self.optimizerWindow.show)
-            menu.addAction(optimize_action)
+        # if level == 0:
+        #     optimize_action = QAction('Optimize', self)
+        #     optimize_action.triggered.connect(self.optimizerWindow.show)
+        #     menu.addAction(optimize_action)
 
 
         selectedItem = menu.exec_(globalPos)
 
     def get_selected_level(self):
+        ''' Return the level of the currently selected item. '''
         indexes = self.treeWidget.selectedIndexes()
         level = 0
         index = indexes[0]
@@ -215,24 +229,10 @@ class MainFrame(QWidget):
             level += 1
         return level
 
-    def get_selected_key(self):
-        item = self.treeWidget.currentItem()
-        input = item.text(0)
-        device = item.parent().text(0)
-        key = device + '.' + input
-        print(key)
-        return key
 
-    def get_selected_keys(self):
-        indexes = self.treeWidget.selectedIndexes()
-        keys = []
-        for i in indexes:
-            if i.column() == 0:
-                full_name = i.parent().data() + '.' + i.data()
-                keys.append(full_name)
-        return keys
 
     def get_selected_state(self):
+        ''' Build a substate from all currently selected items. '''
         indexes = self.treeWidget.selectedIndexes()
         state = {}
         for i in indexes:
