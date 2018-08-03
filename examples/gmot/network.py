5508bc76ed99cdc7008b391cec63f691b9b13e2e
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-08-03 13:05:36 -0600

    Add correct LabJack devid

--- examples/gmot/network.py
+++ examples/gmot/network.py
@@ -3,7 +3,7 @@ from devices.labjackT7 import LabJack
 from devices.picoAmp import PicoAmp
 from __main__ import *
 
-devid = '160049673'
+devid = '470016934'
 labjack = LabJack(devid=devid)
 control = AutoAlign(name='control', labjack=labjack, path='examples/%s'%sys.argv[1])
 mems = PicoAmp('MEMS', labjack, parent=control)
