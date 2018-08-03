8d8ffa8eeb3279d6b0ee62cb394b095aa63506c1
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-02 20:05:40 -0600

    Initial commit

---
+++ utility.py
@@ -0,0 +1,12 @@
+import inspect
+
+def methodsWithDecorator(cls, decoratorName):
+    methods = []
+    sourcelines = inspect.getsourcelines(cls)[0]
+    for i,line in enumerate(sourcelines):
+        line = line.strip()
+        if line.split('(')[0].strip() == '@'+decoratorName: # leaving a bit out
+            nextLine = sourcelines[i+1]
+            name = nextLine.split('def')[1].split('(')[0].strip()
+            methods.append(name)
+    return methods
