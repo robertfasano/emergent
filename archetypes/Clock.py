de7e894f53909c7d0663e367f668ccc1fe172d80
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-04 10:06:15 -0600

    Add/improve documentation

--- archetypes/Clock.py
+++ archetypes/Clock.py
@@ -1,9 +1,14 @@
 import time
 import numpy as np
 from threading import Thread
-from archetypes.Parallel import ProcessHandler
+from archetypes.parallel import ProcessHandler
 
 class Clock(ProcessHandler):
+    ''' The Clock class attaches to a Control node to provide sequencing capabilities:
+        waveforms can be specified for any connected Input nodes, and the Clock
+        combines all input sequences into a master sequence describing the
+        state of all sequenced Input nodes at each point during the experimental
+        cycle. '''
     def __init__(self, parent):
         ''' Initializes the Clock and attaches it to a parent Control node '''
         ProcessHandler.__init__(self)
@@ -12,19 +17,27 @@ class Clock(ProcessHandler):
         self.state = {}
 
     def add_input(self, input):
+        ''' Connects an Input node to the Clock, allowing sequencing. '''
         self.inputs.append(input)
 
     def start(self, T):
+        ''' Prepares and runs the master sequence. To reduce timing jitter,
+        two threads are used: one which updates the internal Clock state at all times
+        (very computationally quick) and another which actuates the Control node
+        to keep in sync with the Clock state (more time-intensive).'''
         states = self.prepare_sequence(T)
         self._run_thread(self.loop)
         self._run_thread(self.sync)
 
     def stop(self):
+        ''' Terminates sequencing. '''
         self._quit_thread(self.loop)
         self._quit_thread(self.sync)
 
     def loop(self, stopped):
-        ''' Loops through the specified sequence. The argument states is a list of tuples where the first element of each tuple is a delay and the second is a target state.'''
+        ''' Loops through the specified sequence. The argument states is a list
+        of tuples where the first element of each tuple is a delay and the second
+        is a target state.'''
         i = 0
         while not stopped():
             delay = self.sequence[i][0]
@@ -34,14 +47,14 @@ class Clock(ProcessHandler):
             i = (i+1)%len(self.sequence)
 
     def sync(self, stopped):
-        ''' Keeps the physical state synced with another internal variable which can be set from another thread. This is useful for running sequences without additional delay due to actuation. '''
+        ''' Keeps the Control state synced with the Clock state.  '''
         while not stopped():
             parent_substate = dict((k, self.parent.state[k]) for k in self.state.keys())
             if self.state != parent_substate:
                 self.parent.actuate(self.state)
 
     def prepare_sequence(self, T):
-        ''' Prepares a master sequence by combining sequences of all inputs '''
+        ''' Prepares a master sequence by combining sequences of all inputs. '''
         times = []
         for input in self.inputs:
             times.extend([x[0]*T for x in input.sequence])
