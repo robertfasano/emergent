57dba334d67ab48ee7a52e8ea98f9d7eb11b25cc
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-04 00:37:56 -0600

    Add coil driver and mot control node

--- examples/gmot/network.py
+++ examples/gmot/network.py
@@ -20,7 +20,10 @@ labjack_slowing = LabJack(devid=devid)
 autoAlign_slowing = AutoAlign(name='autoAlign_slowing', labjack=labjack_slowing, path='examples/%s'%sys.argv[1])
 mems_slowing = PicoAmp('mems_slowing', labjack_slowing, parent=autoAlign_slowing)
 
-# ''' Define coil drivers '''
-# devid = '470016973'
-# labjack_MOT = LabJack(devid=devid)
-# coils = CurrentDriver('coils', port1, port2, parent = control, labjack = labjack_MOT)
+''' Define coil drivers '''
+devid = '470016973'
+port1 = None
+port2 = None
+labjack_MOT = LabJack(devid=devid)
+mot = MOT(name='MOT', labjack=labjack_MOT,path='examples/%s'%sys.argv[1])
+coils = CurrentDriver('coils', port1, port2, parent = mot, labjack = labjack_MOT)
