89102fceea1c065a63a3157034fee2da5c910c83
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-04 12:31:50 -0600

    Improve docs.

--- controls/autoAlign.py
+++ controls/autoAlign.py
@@ -11,15 +11,21 @@ sys.path.append(char.join(os.getcwd().split(char)[0:-1]))
 from utility import cost
 
 class AutoAlign(Control):
+    ''' Control node for automated fiber alignment. A Labjack T7 is used both for
+        MEMS mirror actuation via SPI and for measurement of the transmitted power. '''
     def __init__(self, name, labjack, parent = None, path='.'):
         super().__init__(name, parent = parent, path=path)
         self.labjack = labjack
 
     def readADC(self, num = 10, delay = 0):
+        ''' Reads the transmitted power from Labjack channel AIN0 with an optional
+            delay. num samplings can be averaged together to improve the signal to
+            noise. '''
         time.sleep(delay)
         return self.labjack.AIn(0, num=num)
 
     @cost
     def measure_power(self, state):
+        ''' Moves to the target alignment and measures the transmitted power. '''
         self.actuate(state)
         return -self.readADC()
--- devices/testDevice.py
+++ devices/testDevice.py
@@ -1,11 +1,17 @@
 from archetypes.node import Device
 from utility import dev
 
-@dev 
+@dev
 class TestDevice(Device):
-        def __init__(self, name, parent):
-                super().__init__(name, parent)
-                self.add_input('X')
-                self.add_input('Y')
-        def _actuate(self, state):
-                print(state)
+    ''' Device driver for the virtual network in the 'basic' example. '''
+    def __init__(self, name, parent):
+        ''' Register with the network and create two Input nodes, 'X' and 'Y'. '''
+        super().__init__(name, parent)
+        self.add_input('X')
+        self.add_input('Y')
+
+    def _actuate(self, state):
+        ''' Usually this method would change a physical state, but for our
+            virtual network we only print the argument (note: the virtual state
+            is updated within the public calling method Device.actuate()).'''
+        print(state)
