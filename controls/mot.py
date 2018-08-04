273ff3028c0833e3a713417b593e3080a3d70d4e
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-04 00:41:33 -0600

    Initial commit

---
+++ controls/mot.py
@@ -0,0 +1,7 @@
+from emergent.archetypes.node import Control
+
+
+class MOT(Control):
+    def __init__(self, name, labjack, parent = None, path='.'):
+        super().__init__(name, parent = parent, path=path)
+        self.labjack = labjack
