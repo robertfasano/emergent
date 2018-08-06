6c274592869d462314a94624b75c84fa4136dc35
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-08-06 16:20:26 -0600

    Add MOT control and coils device

--- examples/gmot/network.py
+++ examples/gmot/network.py
@@ -1,13 +1,9 @@
 from controls.autoAlign import AutoAlign
+from controls.mot import MOT
 from devices.labjackT7 import LabJack
 from devices.picoAmp import PicoAmp
+from devices.current_driver import CurrentDriver
 from __main__ import *
-
-# devid = '470016934'
-# labjack_cooling = LabJack(devid=devid)
-# control = AutoAlign(name='control', labjack=labjack_cooling, path='examples/%s'%sys.argv[1])
-# mems = PicoAmp('MEMS', labjack_cooling, parent=control)
-
 
 ''' Define autoAlign '''
 devid = '470016934'
@@ -20,10 +16,11 @@ labjack_slowing = LabJack(devid=devid)
 autoAlign_slowing = AutoAlign(name='autoAlign_slowing', labjack=labjack_slowing, path='examples/%s'%sys.argv[1])
 mems_slowing = PicoAmp('mems_slowing', labjack_slowing, parent=autoAlign_slowing)
 
+
 ''' Define coil drivers '''
 devid = '470016973'
 port1 = None
 port2 = None
 labjack_MOT = LabJack(devid=devid)
 mot = MOT(name='MOT', labjack=labjack_MOT,path='examples/%s'%sys.argv[1])
-coils = CurrentDriver('coils', port1, port2, parent = mot, labjack = labjack_MOT)
+coils = CurrentDriver('coils', 'COM13', 'COM18', parent = mot, labjack = labjack_MOT)
