13087ace47cbe4f45a1de3e9c4b033688d7d3ae5
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-05 19:55:13 -0600

    Add onLoad() method to prepare sequence after everything is loaded

--- examples/basic/process.py
+++ examples/basic/process.py
@@ -1,7 +1,8 @@
 from __main__ import *
 
-deviceA.children['X'].sequence = [(0,0), (0.5, 1)]
-deviceA.children['Y'].sequence = [(0,0), (0.25,-1), (0.5,0), (0.75, 1)]
-control.clock.add_input(deviceA.children['X'])
-control.clock.add_input(deviceA.children['Y'])
+# deviceA.children['X'].sequence = [(0,0), (0.5, 1)]
+# deviceA.children['Y'].sequence = [(0,0), (0.25,-1), (0.5,0), (0.75, 1)]
+# deviceB.children['X'].sequence = [(0,1)]
+# deviceB.children['Y'].sequence = [(0,0)]
+control.onLoad()
 #control.clock.start(3)
