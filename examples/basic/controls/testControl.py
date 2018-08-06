7a194b51c39846341e93c5b399133a45af622d6c
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-05 22:36:49 -0600

    Add sequence optimization

--- examples/basic/controls/testControl.py
+++ examples/basic/controls/testControl.py
@@ -20,16 +20,23 @@ class TestControl(Control):
             return -np.exp(-(x-0.5)**2/x0**2)*np.exp(-(y-0.5)**2/y0**2)
 
         @cost
-        def cost_ramp(self, arr):
+        def cost_ramp(self, sequence):
             ''' Evaluate a cost function similar to an optical scattering force,
                 which is maximized for a ramp x(t)=1/t. '''
+            self.sequences = sequence
+            s = sequence['deviceA.X']
             result = 0
-            for i in range(len(arr)):
-                x = arr[i]
-                t = i/10
+            for i in range(len(s)):
+                x = s[i][1]
+                t = s[i][0]
                 result -= 1/(1+(1-x*t)**2)
             return result
 
+        def optimize_sequence(self):
+            self.clock.prepare_constant(1, 'deviceA.X', 15)
+            s = self.get_subsequence(['deviceA.X'])
+            self.optimizer.scipy_minimize(s, self.cost_ramp, params={'method':'Nelder-Mead', 'tol':4e-3, 'plot':0})
+            self.optimizer.animate_sequence_history()
 
 
         def scramble(self):
