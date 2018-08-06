f1c39e07ab5de3ea01829d4c836376883481a4ee
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-05 19:51:57 -0600

    Add cycle_time as property of clock; store sequence in parent; fix other bugs

--- archetypes/Clock.py
+++ archetypes/Clock.py
@@ -13,22 +13,17 @@ class Clock(ProcessHandler):
         ''' Initializes the Clock and attaches it to a parent Control node '''
         ProcessHandler.__init__(self)
         self.parent = parent
-        self.inputs = []
         self.state = {}
+        self.cycle_time = None
 
-    def add_input(self, input):
-        ''' Connects an Input node to the Clock, allowing sequencing. '''
-        self.inputs.append(input)
-
-    def start(self, T):
+    def start(self):
         ''' Prepares and runs the master sequence. To reduce timing jitter,
         two threads are used: one which updates the internal Clock state at all times
         (very computationally quick) and another which actuates the Control node
         to keep in sync with the Clock state (more time-intensive).'''
-        states = self.prepare_sequence(T)
+        states = self.prepare_sequence()
         self._run_thread(self.sync)
         self._run_thread(self.loop)
-
 
     def stop(self):
         ''' Terminates sequencing. '''
@@ -41,26 +36,28 @@ class Clock(ProcessHandler):
         is a target state.'''
         i = 0
         while not stopped():
-            delay = self.sequence[i][0]
-            state = self.sequence[i][1]
+            delay = self.parent.sequence[i][0]
+            state = self.parent.sequence[i][1]
             time.sleep(delay)
             self.state = state
-            i = (i+1)%len(self.sequence)
+            i = (i+1)%len(self.parent.sequence)
 
-    def run_once(self, T):
-        states = self.prepare_sequence(T)
+    def run_once(self):
+        ''' Prepare a sequence based on the input nodes with a total time T.
+            Runs the sequence once. '''
+        states = self.prepare_sequence()
         self._run_thread(self.sync)
         self.single_shot()
         self._quit_thread(self.sync)
 
     def single_shot(self):
-        ''' Executes the sequence once '''
+        ''' Executes the sequence once. '''
         i = 0
-        for i in range(len(sequence)):
-            delay = self.sequence[i][0]
-            state = self.sequence[i][1]
-            time.sleep(delay)
+        for i in range(len(self.parent.sequence)):
+            delay = self.parent.sequence[i][0]
+            state = self.parent.sequence[i][1]
             self.state = state
+            time.sleep(delay)
 
     def sync(self, stopped):
         ''' Keeps the Control state synced with the Clock state.  '''
@@ -69,10 +66,12 @@ class Clock(ProcessHandler):
             if self.state != parent_substate:
                 self.parent.actuate(self.state)
 
-    def prepare_sequence(self, T):
+    def prepare_sequence(self):
         ''' Prepares a master sequence by combining sequences of all inputs. '''
+        T = self.parent.cycle_time
         times = []
-        for input in self.inputs:
+        for input in self.parent.inputs.values():
+            self.parent.sequences[input.full_name] = input.sequence 
             times.extend([x[0]*T for x in input.sequence])
         times = np.unique(times)
 
@@ -81,10 +80,10 @@ class Clock(ProcessHandler):
         for i in range(len(times)):
             t = times[i]
             state = {}
-            for input in self.inputs:
+            for input in self.parent.inputs.values():
                 input_times = [x[0]*T for x in input.sequence]
                 current_index = np.where(input_times <= t)[0][-1]
                 state[input.full_name] = input.sequence[current_index][1]
             states.append((delays[i], state))
 
-        self.sequence = states
+        self.parent.sequence = states
