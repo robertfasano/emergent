27b7c084c505391d787451e5fee0e330034c631d
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-06 23:45:41 -0600

    Move real/virtual input functions to node.Device

--- networks/example/devices/testDevice.py
+++ networks/example/devices/testDevice.py
@@ -15,69 +15,30 @@ class TestDevice(Device):
         self.add_input('u', type='virtual')
         self.add_input('v', type='virtual')
 
-    def _actuate(self, state, type='any'):
+    def _actuate(self, state):
         ''' Usually this method would change a physical state, but for our
             virtual network we only print the argument (note: the virtual state
             is updated within the public calling method Device.actuate()).'''
-
-        ''' If state contains mixed real/virtual inputs, only actuate the real '''
         print(state)
-        real = self.is_real(state)
-        virtual = self.is_virtual(state)
-        if real and virtual:
-            state = self.get_real(state)
-            virtual = False
-        if real and type is not 'real':
-            self.update(self.real_to_virtual(state))
-        elif type is not 'real':
-            self.update(self.virtual_to_real(state))
-
-    def get_real(state):
-        new_state = {}
-        for key in state.keys():
-            if self.children[key].type == 'real':
-                new_state[key] = state[key]
-        return new_state
-
-    def is_real(self, state):
-        ''' Return True if all elements of the state are real.'''
-        real = True
-        for key in state.keys():
-            real = real and (self.children[key].type == 'real')
-        return real
-
-    def is_virtual(self, state):
-        ''' Return True if all elements of the state are real.'''
-        virtual = True
-        for key in state.keys():
-            virtual = virtual and (self.children[key].type == 'virtual')
-        return virtual
+        ''' this is where you would do some actuation on the real state '''
 
     def real_to_virtual(self, state):
-        try:
-            X = state['X']
-        except KeyError:
-            X = self.state['X']
-        try:
-            Y = state['Y']
-        except KeyError:
-            Y = self.state['Y']
+        ''' Converts a real state to a virtual state. '''
+        state = self.get_missing_keys(state, ['X','Y'])
+        X = state['X']
+        Y = state['Y']
+
         theta = 30*np.pi/180
         u = X*np.cos(theta)-Y*np.sin(theta)
         v = X*np.sin(theta)+Y*np.cos(theta)
         virtual = {'u':u, 'v':v}
-        print(virtual)
         return virtual
 
     def virtual_to_real(self, state):
-        try:
-            u = state['u']
-        except KeyError:
-            u = self.state['u']
-        try:
-            v = state['v']
-        except KeyError:
-            v = self.state['v']
+        ''' Converts a virtual state to a real state. '''
+        state = self.get_missing_keys(state, ['u','v'])
+        u = state['u']
+        v = state['v']
 
         theta = 30*np.pi/180
         X = u*np.cos(-theta)-v*np.sin(-theta)
