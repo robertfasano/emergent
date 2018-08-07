17e1447c3599375f553e50d1667ee63f465dbd89
rjf2 <robert.fasano@nist.gov>
2018-08-07 15:30:34 -0600

    Add stream() method for testing LabJack output stream

--- controls/mot.py
+++ controls/mot.py
@@ -4,3 +4,11 @@ class MOT(Control):
     def __init__(self, name, labjack, parent = None, path='.'):
         super().__init__(name, parent = parent, path=path)
         self.labjack = labjack
+
+    def stream(self, period = 1, amplitude = 0.1):
+        key='coils.I1'
+        self.cycle_time = period
+        self.inputs[key].sequence = [(0,0), (period/2, amplitude)]
+        data = self.clock.prepare_stream(key)
+
+        self.labjack.stream_out(0, data)
