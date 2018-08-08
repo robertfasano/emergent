dd114e711a701cc9d999715b80323b6b6a4cff83
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-08-08 17:27:24 -0600

    Add intensity servo

--- networks/gmot/network.py
+++ networks/gmot/network.py
@@ -4,6 +4,7 @@ from controls.mot import MOT
 from devices.labjackT7 import LabJack
 from devices.picoAmp import PicoAmp
 from devices.current_driver import CurrentDriver
+from devices.intensity_servo import IntensityServo
 from __main__ import *
 
 ''' Define autoAlign '''
@@ -11,14 +12,16 @@ devid = '470016934'
 labjack_cooling = LabJack(devid=devid)
 autoAlign_cooling = AutoAlign(name='autoAlign_cooling', labjack=labjack_cooling, path='networks/%s'%sys.argv[1])
 mems_cooling = PicoAmp('mems_cooling', labjack_cooling, parent=autoAlign_cooling)
+servo_cooling = IntensityServo('servo_cooling', labjack_cooling, 0, 1, parent = autoAlign_cooling)
 
 devid = '470016970'
 labjack_slowing = LabJack(devid=devid)
 autoAlign_slowing = AutoAlign(name='autoAlign_slowing', labjack=labjack_slowing, path='networks/%s'%sys.argv[1])
 mems_slowing = PicoAmp('mems_slowing', labjack_slowing, parent=autoAlign_slowing)
+servo_slowing = IntensityServo('servo_slowing', labjack_slowing, 0, 1, parent = autoAlign_slowing)
 
 
-''' Define coil drivers '''
+''' Define MOT control hub '''
 devid = '470016973'
 port1 = None
 port2 = None
