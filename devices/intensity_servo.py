af1d440ca8e327c75ab97c5b85c8ecd39421a30c
Robert Fasano <robert.j.fasano@colorado.edu>
2018-08-07 19:25:46 -0600

    Finish writing class

--- devices/intensity_servo.py
+++ devices/intensity_servo.py
@@ -1,12 +1,26 @@
 from emergent.archetypes.node import Device
 
 class IntensityServo(Device):
-    def __init__(self, name, labjack, parent = None):
+    ''' Device driver for the NIST OFM intensity servo. Designed to control a
+        single channel of the board using both outputs of a LabJack LJTick-DAC. '''
+    def __init__(self, name, labjack, setpoint_channel, lock_channel, parent = None):
         super().__init__(name, parent = parent)
         self.labjack = labjack
+        self.setpoint_channel = setpoint_channel
+        self.lock_channel = lock_channel
 
-    def set(self, setpoint, value):
+    def _actuate(state):
+        ''' Sets setpoint via analog out control.
 
-    def select(self, setpoint):
+            Args:
+                state (dict): State dict of the form {'V':1}
+        '''
+        self.labjack.AOut(self.setpoint_channel, state['V'], HV=True)
 
-    def toggle(self):
+    def gain(self, state):
+        ''' Turns the gain on or off.
+
+            Args:
+                state (int): 0 or 1
+        '''
+        self.labjack.AOut(self.lock_channel, state*3.3, HV=True)
