d2646c5fcad2719990b35849eeca0e236278694f
Robbie Fasano <robert.fasano@nist.gov>
2018-08-27 14:55:44 -0600

    Separate pulsed_cost into pulsed slowing or field methods

--- emergent/controls/mot.py
+++ emergent/controls/mot.py
@@ -22,19 +22,34 @@ class MOT(Control):
         self.labjack.stream_out(0, data)
 
     @cost
-    def pulsed_cost(self, state):
+    def pulsed_slowing(self, state):
         ''' Toggle between high and low magnetic field; measure mean fluorescence
             in both cases and return the difference. '''
-        # self.actuate({'coils.I1':0, 'coils.I2':0})
-        self.actuate({'coils.grad':0, 'coils.zero':0})
-
-        time.sleep(0.1)
-        low = self.labjack.streamburst(duration=0.05, operation = 'mean')
         self.actuate(state)
-        time.sleep(0.1)
+        self.labjack.AOut(0,0)
+        time.sleep(0.05)
+        self.labjack.AOut(1, 0) # output DC level for subtraction with SRS
+        low = self.labjack.streamburst(duration=0.05, operation = 'mean')
+        self.labjack.AOut(1, low) # output DC level for subtraction with SRS
+        self.labjack.AOut(0,3.3)
+        time.sleep(0.05)
         high = self.labjack.streamburst(duration=0.05, operation = 'mean')
+        return -high    # low is subtracted out by SRS
 
-        return -(high-low)
+    @cost
+    def pulsed_field(self, state):
+        ''' Toggle between high and low magnetic field; measure mean fluorescence
+            in both cases and return the difference. '''
+        self.actuate({'coils.I1':0, 'coils.I2':0})
+        time.sleep(0.05)
+        self.labjack.AOut(1, 0) # output DC level for subtraction with SRS
+        low = self.labjack.streamburst(duration=0.1, operation = 'mean')
+        self.labjack.AOut(1, low) # output DC level for subtraction with SRS
+        self.actuate(state)
+        time.sleep(0.05)
+        high = self.labjack.streamburst(duration=0.1, operation = 'mean')
+        return -high    # low is subtracted out by SRS
+
 
     def wave(self, frequency=2):
         V = 3.3
