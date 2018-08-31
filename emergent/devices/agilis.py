dca6b057c65597d65110c8443bad2b95e6e6ce8b
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-08-30 18:09:55 -0600

    Fix bugs to allow persistent semi-absolute positioning (relative after absolute calibration)

--- emergent/devices/agilis.py
+++ emergent/devices/agilis.py
@@ -22,7 +22,7 @@ class Agilis(Device):
         if connect:
             self._connected = self._connect()
         self.mirror=None
-        self.range = {}
+        self.range = {'X1':377150, 'Y1':310100, 'X2':250800, 'Y2':250550}
 
     def _connect(self):
         self.serial = serial.Serial(port = self.port, baudrate = 921600, parity = ser.PARITY_NONE, stopbits = ser.STOPBITS_ONE, bytesize = ser.EIGHTBITS, timeout = 1, encoding = 'ascii', name = 'Agilis')
@@ -32,66 +32,52 @@ class Agilis(Device):
             self.saved_positions = {}
             self.mirrors = [1,2]
             self._connected = 1
+            self.step_size = 50
             for mirror in [1,2]:
                 self.set_channel(mirror)
                 for axis in [1,2]:
-                    self.command('%sSU+50'%axis)
-                    self.command('%sSU-50'%axis)
+                    self.command('%sSU+%i'%(axis, self.step_size))
+                    self.command('%sSU-%i'%(axis, self.step_size))
             self.set_channel(1)
-
 
     def _actuate(self, state):
         ''' Software-based absolute positioning, achieved by moving relative to a known last position '''
+        ''' Pass in a value from -1 to 1 for each axis; autoscaling to calibrated range is done before
+            actuation '''
+        if self.range == {}:
+            return
         indices = {'X1':0, 'Y1':1, 'X2':2, 'Y2':3}
         for input in state:
             index = indices[input]
             mirror = int(input[1])
             axis = {'X':1,'Y':2}[input[0]]
-            ''' If position is unknown for a given axis, check it first '''
-            if self.state[input] is None:
-                self.state[input] = 0
-            else:
-                step = state[input] - self.state[input]
-                if step != 0:
-                    self.relative_move(mirror, axis, step)
+
+            step = state[input] - self.state[input]
+            unnorm_step = step/2 * (1+self.range[input]/self.step_size)
+            if step != 0:
+                self.relative_move(mirror, axis, unnorm_step)
+                print('step:', step, 'unnorm_step:', unnorm_step)
 
     def clear_buffer(self):
         time.sleep(.1)
         while True:
             if self.serial.read() == '':
                 break
+
     def calibrate(self):
-        step_size = 50
         for mirror in [1,2]:
             self.set_channel(mirror)
             for axis in [1,2]:
                 try:
                     self.clear_buffer()
-                    # print('Moving to center')
-                    # self.set_position(mirror, axis, 500)
-                    # while True:
-                    #     r = self.command('%iTS'%axis)
-                    #     if r == '%iTS0'%axis:
-                    #         break
-                    print('Moving to negative limit')
                     self.command('%sMV-3'%axis)     # move to negative limit
-                    while True:
-                        r=self.command('PH?')
-                        if r != 'PH0':
-                            break
+                    self.wait_until_stopped(axis)
                     self.clear_buffer()
-                    print('Limit reached')
-                    self.command('%sSU%i'%(axis, step_size))  # set step size in positive direction
-                    self.command('%sSU%i'%(axis, -step_size))  # set step size in negative direction
                     self.command('%sZP'%axis)   # clear step counter
                     self.command('%sPR100'%axis)    # move 100 steps out of limit
-                    print('Moving to positive limit')
+                    self.wait_until_stopped(axis)
                     self.command('%sMV3'%axis)  # move to positive limit
-                    while True:
-                        if self.command('PH?')!='PH0':
-                            self.serial.read()
-                            break
-                    print('Limit reached')
+                    self.wait_until_stopped(axis)
                     try:
                         check_steps = self.command('%sTP?'%axis)
                         steps = int(check_steps.split('%sTP'%axis)[1])
@@ -99,26 +85,25 @@ class Agilis(Device):
                         self.clear_buffer()
                         check_steps = self.command('%sTP?'%axis)
                         steps = int(check_steps.split('%sTP'%axis)[1])
-                    self.command('%sPR-100'%axis)    # move 100 steps out of limit
-                    axis_range = step_size*steps
+                    axis_range = self.step_size*steps
                     self.axis_label = {1:'X',2:'Y'}[axis]
                     self.label = self.axis_label + str(mirror)
                     self.range[self.label] = axis_range
                     print('Measured axis range: %f'%axis_range)
-                    # self.set_position(mirror, axis, 500)
-                    print('Returning to zero')
-                    steps_to_zero = int(axis_range/2/step_size)
+                    steps_to_zero = int(axis_range/2/self.step_size)
                     self.command('%sPR%i'%(axis,-steps_to_zero))
+                    self.wait_until_stopped(axis)
                     self.clear_buffer()
                 except Exception as e:
                     print(e)
                     print(self.command('TE?'))
+        for input in self.state:
+            self.state[input] = 0
 
     def command(self, cmd, reply = True):
         r = self.serial.command(cmd, suffix = '\r\n', reply = reply)
         if reply:
             return r.split('\r')[0]
-
 
     def cost(self, num = 1):
         vals = []
@@ -132,10 +117,11 @@ class Agilis(Device):
 
     def set_channel(self, mirror):
         if self.mirror != mirror:
-            self.command('CC%i'%mirror)
-            print('Mirror now on channel %i'%mirror)
-        else:
-            print('Mirror already on channel %i'%mirror)
+            while True:
+                self.command('CC%i'%mirror)
+                actual = self.command('CC?')
+                if int(actual[2]) == mirror:
+                    break
         self.mirror = mirror
 
     def get_position(self, mirror, axis=None):
@@ -156,6 +142,11 @@ class Agilis(Device):
                     pos.append(self.get_position(mirror, axis))
             return pos
 
+    def wait_until_stopped(self, axis):
+        while True:
+            if self.command('%iTS?'%axis) == '%iTS0'%axis:
+                return
+
     def set_position(self, mirror, axis, position):
         if type(position) == int:
             self.set_channel(mirror)
@@ -172,7 +163,6 @@ class Agilis(Device):
             Unsaved axes are written as -1 '''
         self.saved_positions[index] = self.get_position(mirror='all')
 
-
     def walk(self):
         print('Entering walk mode.')
         step = 100
@@ -188,7 +178,6 @@ class Agilis(Device):
             elif command == 'v':
                 step /= 2
                 print('Decreasing step')
-
 
             elif command == 's':
                 self.relative_move(1, 1, step = step)
