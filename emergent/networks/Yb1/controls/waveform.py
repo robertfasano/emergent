9626ace18db8b1d04c6bfad5cc5edaae254d782c
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-10-02 17:34:13 -0600

    Re-add _connect() method and create new ExponentialRamp class

--- emergent/networks/Yb1/controls/waveform.py
+++ emergent/networks/Yb1/controls/waveform.py
@@ -4,10 +4,11 @@ import numpy as np
 import time
 
 class LinearRamp(Device):
-    def __init__(self, name, duration, labjack, parent = None, trigger = None):
+    def __init__(self, name, duration, labjack, parent = None, steps = 1000, trigger = None):
         super().__init__(name=name, parent = parent)
         self.duration = duration
         self.labjack = labjack
+        self.steps = steps
         self.add_input('V0')
         self.add_input('Vf')
         self.labjack.prepare_stream_out(['DAC0'], trigger=trigger)
@@ -19,7 +20,35 @@ class LinearRamp(Device):
             slope = (state['Vf']-state['V0'])/self.duration
             t = np.linspace(0,self.duration, 100)
             y = state['V0'] + slope*t
-            sequence, scanRate = self.labjack.resample(y, self.duration, max_samples = 500)
+            sequence, scanRate = self.labjack.resample(y, self.duration, max_samples = self.steps)
             self.labjack.stream_out([0], sequence, scanRate, loop=0)
         else:
             self.initialized = 1
+
+    def _connect(self):
+        return self.labjack._connected
+
+
+class ExponentialRamp(Device):
+    def __init__(self, name, duration, labjack, parent = None, steps = 1000, trigger = None):
+        super().__init__(name=name, parent = parent)
+        self.duration = duration
+        self.labjack = labjack
+        self.steps = steps
+        self.add_input('V0')
+        self.add_input('tau')
+        self.labjack.prepare_stream_out(['DAC0'], trigger=trigger)
+        self.initialized = 0
+
+    def _actuate(self, state):
+        state = self.get_missing_keys(state, None)
+        if self.initialized:
+            t = np.linspace(0,self.duration, 100)
+            y = state['V0']*np.exp(-t/state['tau'])
+            sequence, scanRate = self.labjack.resample(y, self.duration, max_samples = self.steps)
+            self.labjack.stream_out([0], sequence, scanRate, loop=0)
+        else:
+            self.initialized = 1
+
+    def _connect(self):
+        return self.labjack._connected
