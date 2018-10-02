f46f35bd3974ccd97b353d510d4f88f65a340252
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-10-02 17:36:42 -0600

    Merge LinearRamp and ExponentialRamp into single Ramp class

--- emergent/networks/Yb1/controls/waveform.py
+++ emergent/networks/Yb1/controls/waveform.py
@@ -3,48 +3,31 @@ import logging as log
 import numpy as np
 import time
 
-class LinearRamp(Device):
-    def __init__(self, name, duration, labjack, parent = None, steps = 1000, trigger = None):
+class Ramp(Device):
+    def __init__(self, name, duration, labjack, type = 'linear', parent = None, steps = 1000, trigger = None):
         super().__init__(name=name, parent = parent)
         self.duration = duration
         self.labjack = labjack
         self.steps = steps
-        self.add_input('V0')
-        self.add_input('Vf')
+        self.type = type
+        if type is 'linear':
+            self.add_input('V0')
+            self.add_input('Vf')
+        elif type is 'exponential':
+            self.add_input('V0')
+            self.add_input('tau')
         self.labjack.prepare_stream_out(['DAC0'], trigger=trigger)
         self.initialized = 0
 
     def _actuate(self, state):
         state = self.get_missing_keys(state, None)
         if self.initialized:
-            slope = (state['Vf']-state['V0'])/self.duration
-            t = np.linspace(0,self.duration, 100)
-            y = state['V0'] + slope*t
-            sequence, scanRate = self.labjack.resample(y, self.duration, max_samples = self.steps)
-            self.labjack.stream_out([0], sequence, scanRate, loop=0)
-        else:
-            self.initialized = 1
-
-    def _connect(self):
-        return self.labjack._connected
-
-
-class ExponentialRamp(Device):
-    def __init__(self, name, duration, labjack, parent = None, steps = 1000, trigger = None):
-        super().__init__(name=name, parent = parent)
-        self.duration = duration
-        self.labjack = labjack
-        self.steps = steps
-        self.add_input('V0')
-        self.add_input('tau')
-        self.labjack.prepare_stream_out(['DAC0'], trigger=trigger)
-        self.initialized = 0
-
-    def _actuate(self, state):
-        state = self.get_missing_keys(state, None)
-        if self.initialized:
-            t = np.linspace(0,self.duration, 100)
-            y = state['V0']*np.exp(-t/state['tau'])
+            t = np.linspace(0,self.duration, 1000)
+            if self.type is 'linear':
+                slope = (state['Vf']-state['V0'])/self.duration
+                y = state['V0'] + slope*t
+            elif self.type is 'exponential':
+                y = state['V0']*np.exp(-t/state['tau'])
             sequence, scanRate = self.labjack.resample(y, self.duration, max_samples = self.steps)
             self.labjack.stream_out([0], sequence, scanRate, loop=0)
         else:
