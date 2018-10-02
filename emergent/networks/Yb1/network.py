65137a4a2cc5b4884a0c9260a1da7238ef684474
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-10-02 16:45:58 -0600

    Switch to two LabJacks; rename ramp generator device

--- emergent/networks/Yb1/network.py
+++ emergent/networks/Yb1/network.py
@@ -1,5 +1,5 @@
 from emergent.networks.Yb1.controls.lattice import Lattice
-from emergent.networks.Yb1.controls.ramp_generator import IntensityRamp
+from emergent.networks.Yb1.controls.waveform import LinearRamp
 
 from emergent.devices.labjackT7 import LabJack
 
@@ -7,7 +7,10 @@ from __main__ import *
 
 ''' Define Lattice '''
 lattice = Lattice(name='lattice', path='networks/%s'%sys.argv[1])
-labjack = LabJack(devid='470016973', name='labjack', parent = lattice)
-lattice.add_labjack(labjack)
+lattice.ljIN = LabJack(devid='470016973', name='labjack')
+lattice.ljIN.prepare_streamburst(channel=0, trigger = 0, max_samples = lattice.max_samples)
 
-ramp = IntensityRamp('intensity_ramp', .03, labjack, parent=lattice)
+lattice.ljOUT = LabJack(devid='470016970', name='labjack')
+lattice.ljOUT.prepare_stream_out(['DAC0'], trigger = 0)
+
+intensity_ramp = LinearRamp('intensity_ramp', .03, lattice.ljOUT, parent=lattice)
