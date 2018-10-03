3c2c7159cf49ea7c12cb7f88088c692f624380e1
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-10-03 10:39:57 -0600

    Rename inputs to be more descriptive

--- emergent/networks/Yb1/controls/waveform.py
+++ emergent/networks/Yb1/controls/waveform.py
@@ -12,13 +12,13 @@ class Ramp(Device):
         self.type = type
         for ch in self.type:
             if self.type[ch] is 'constant':
-                self.add_input('V0_%i'%ch)
+                self.add_input('DAC%i: Constant V0'%ch)
             if self.type[ch] is 'linear':
-                self.add_input('V0_%i'%ch)
-                self.add_input('Vf_%i'%ch)
+                self.add_input('DAC%i: Linear V0'%ch)
+                self.add_input('DAC%i: Linear Vf'%ch)
             elif self.type[ch] is 'exponential':
-                self.add_input('V0_%i'%ch)
-                self.add_input('tau_%i'%ch)
+                self.add_input('DAC%i: Exponential V0'%ch)
+                self.add_input('DAC%i: Exponential tau'%ch)
         channels = ['DAC%i'%i for i in self.type]
         self.labjack.prepare_stream_out(channels, trigger=trigger)
         self.initialized = 0
@@ -30,12 +30,12 @@ class Ramp(Device):
             y = np.zeros((len(t),len(self.type)))
             for ch in self.type:
                 if self.type[ch] is 'constant':
-                    y[:,ch] = state['V0_%i'%ch]
+                    y[:,ch] = state['DAC%i: Constant V0'%ch]
                 elif self.type[ch] is 'linear':
-                    slope = (state['Vf_%i'%ch]-state['V0_%i'%ch])/self.duration
-                    y[:,ch] = state['V0_%i'%ch] + slope*t
+                    slope = (state['DAC%i: Linear Vf'%ch]-state['DAC%i: Linear V0'%ch])/self.duration
+                    y[:,ch] = state['DAC%i: Linear V0'%ch] + slope*t
                 elif self.type[ch] is 'exponential':
-                    y[:,ch] = state['V0_%i'%ch]*np.exp(-t/state['tau_%i'%ch])
+                    y[:,ch] = state['DAC%i: Exponential V0'%ch]*np.exp(-t/state['DAC%i: Exponential tau'%ch])
             sequence, scanRate = self.labjack.resample(y, self.duration, max_samples = self.steps)
             self.labjack.stream_out(list(self.type.keys()), sequence, scanRate, loop=0)
         else:
