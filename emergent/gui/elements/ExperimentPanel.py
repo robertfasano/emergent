41016f49d6a17b5c6a10b4eb6a4030d0e13834be
rjf2 <robert.fasano@nist.gov>
2018-10-11 15:28:09 -0600

    Interface with new HistoryPanel and save optimization results to file

--- emergent/gui/elements/ExperimentPanel.py
+++ emergent/gui/elements/ExperimentPanel.py
@@ -11,6 +11,7 @@ import logging as log
 import time
 from scipy.stats import linregress
 import numpy as np
+import datetime
 
 class OptimizerLayout(QVBoxLayout, ProcessHandler):
     def __init__(self, parent):
@@ -180,9 +181,13 @@ class OptimizerLayout(QVBoxLayout, ProcessHandler):
             log.warn('Select inputs before starting optimization!')
             return
         state = self.parent.treeWidget.get_selected_state()
-
+        cost_name = self.cost_box.currentText()
+        cost = getattr(control, cost_name)
         optimizer, index = control.attach_optimizer(state)
         control.optimizers[index]['status'] = 'Optimizing'
+        t = datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
+        row = self.parent.historyPanel.add_event(t, cost_name, algorithm_name, 'Optimizing', optimizer)
+
         func = getattr(optimizer, algorithm_name.replace(' ','_'))
         params = self.params_edit.toPlainText().replace('\n','').replace("'", '"')
         params = json.loads(params)
@@ -193,8 +198,7 @@ class OptimizerLayout(QVBoxLayout, ProcessHandler):
         cost_params = json.loads(cost_params)
         cost_params['cycles per sample'] = int(self.cycles_per_sample_edit.text())
 
-        cost_name = self.cost_box.currentText()
-        cost = getattr(control, cost_name)
+
         if state == {}:
             log.warn('Please select at least one Input node for optimization.')
         else:
@@ -202,6 +206,8 @@ class OptimizerLayout(QVBoxLayout, ProcessHandler):
             func(state, cost, params, cost_params, self.update_progress_bar)
             log.info('Optimization complete!')
             control.optimizers[index]['status'] = 'Done'
+            self.parent.historyPanel.update_event_status(row, 'Done')
+            optimizer.log(t + ' - ' + cost_name + ' - ' + algorithm_name)
 
             # del control.optimizers[index]
 
