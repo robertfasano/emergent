f817f23df28cda048730be3f58b417c63fe5d432
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-11-02 17:17:25 -0600

    Factor functions into Client class

--- emergent/archetypes/client.py
+++ emergent/archetypes/client.py
@@ -1,16 +1,22 @@
 import asyncio
 import json
 
-async def tcp_echo_client(message, loop):
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
-                                                   loop=loop)
-    writer.write(message.encode())
-    data = await reader.read(1000)
+class Client():
+    def __init__(self):
+        return
 
-    writer.close()
-    return json.loads(data.decode())
+    async def tcp_echo_client(self, message, loop):
+        reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+                                                       loop=loop)
+        writer.write(json.dumps(message).encode())
+        data = await reader.read(1000)
 
-def get_state():
-    loop = asyncio.get_event_loop()
-    state = loop.run_until_complete(tcp_echo_client('get_state', loop))
-    return state
+        writer.close()
+        return json.loads(data.decode())
+
+    def get_state(self):
+        loop = asyncio.get_event_loop()
+        message = {'op': 'get_state'}
+        state = loop.run_until_complete(self.tcp_echo_client(message, loop))
+        print(state)
+        return state
