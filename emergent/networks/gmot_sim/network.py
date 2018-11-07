d1bbf4cfc989411a0a05af620dec60fc5ca7e0bc
Robert Fasano <robert.j.fasano@colorado.edu>
2018-11-06 22:47:55 -0700

    Use new VirtualDevice class for all simulated devices

--- emergent/networks/gmot_sim/network.py
+++ emergent/networks/gmot_sim/network.py
@@ -4,15 +4,13 @@ import sys
 sys.path.append('/Users/rjf2/Documents/Projects/Ultraportable/portable/MOT theory')
 sys.path.insert(0, 'C:\\Users\\Robbie\\Documents\\GitHub\\portable\\MOT theory')
 from parameters import *
-from device import MOTControl, CoilDevice, BeamDevice, GratingDevice
-
-
-
+from device import MOTControl, VirtualDevice
 gmot = MOTControl('gmot', path='networks/%s'%sys.argv[1])
-coil1 = CoilDevice('coil1', gmot)
-coil2 = CoilDevice('coil2', gmot)
-grating = GratingDevice('grating', gmot)
-trapping = BeamDevice('trapping', gmot)
-slowing = BeamDevice('slowing', gmot)
+
+coil1 = VirtualDevice('coil1', gmot, inputs = ['R', 'z', 'N', 'I'])
+coil2 = VirtualDevice('coil2', gmot, inputs = ['R', 'z', 'N', 'I'])
+grating = VirtualDevice('grating', gmot, inputs = ['R1', 'd', 'position'])
+trapping = VirtualDevice('trapping', gmot, inputs = ['power', 'radius', 'detuning', 'polarization'])
+slowing = VirtualDevice('slowing', gmot, inputs = ['power', 'radius', 'detuning', 'polarization'])
 
 gmot.actuate(state)
