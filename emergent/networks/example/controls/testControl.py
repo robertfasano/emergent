29f03e550bb2ebc979a0b5c790431ba5bfe1b951
Robert Fasano <robert.j.fasano@colorado.edu>
2018-11-17 11:00:28 -0700

    Add sign to error params dict

--- emergent/networks/example/controls/testControl.py
+++ emergent/networks/example/controls/testControl.py
@@ -38,7 +38,7 @@ class TestControl(Control):
             return result
 
         @error
-        def error(self, state, error_params = {"drift rate": 1, "proportional_gain": 0.75, "integral_gain": 0.5}):
+        def error(self, state, error_params = {"drift rate": 1, "sign":-1, "proportional_gain": 0.5, "integral_gain": 0.25}):
             self.actuate(state)
             dev = list(state.keys())[0]
             input = list(state[dev].keys())[0]
