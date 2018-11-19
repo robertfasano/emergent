c85280ff23fb09ea85c2dc34110e48bb088cb5ca
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-11-19 13:51:32 -0700

    Factor gui settings load out of prepare_optimizer; allow prepare_optimizer to be called from code, bypassing the gui

--- emergent/gui/elements/ServoPanel.py
+++ emergent/gui/elements/ServoPanel.py
@@ -84,38 +84,50 @@ class ServoLayout(QVBoxLayout, ProcessHandler):
             self.current_control = control
         self.update_algorithm_display()
 
-    def prepare_optimizer(self):
-        ''' Call chosen optimization routine with user-selected cost function and parameters '''
+    def get_settings_from_gui(self):
+        settings = {}
+        settings['state'] = self.parent.treeWidget.get_selected_state()
+        settings['cost_name'] = self.error_box.currentText()
         try:
-            control = self.parent.treeWidget.get_selected_control()
+            settings['control'] = self.parent.treeWidget.get_selected_control()
         except IndexError:
             log.warn('Select inputs before starting optimization!')
             return
-        state = self.parent.treeWidget.get_selected_state()
-        cost_name = self.error_box.currentText()
-        cost = getattr(control, cost_name)
-        optimizer, index = control.attach_optimizer(state, cost)
-        control.optimizers[index]['status'] = 'Servoing'
-        t = datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
-        row = self.parent.historyPanel.add_event(t, cost_name, 'PID', 'Servoing', optimizer)
-        func = optimizer.PID
         params = self.params_edit.toPlainText().replace('\n','').replace("'", '"')
-        params = json.loads(params)
-
+        settings['params'] = json.loads(params)
         error_params = self.error_params_edit.toPlainText().replace('\n','').replace("'", '"')
-        error_params = json.loads(error_params)
+        settings['error_params'] = json.loads(error_params)
 
-        if state == {}:
+        return settings
+
+    def prepare_optimizer(self, *args, settings = {'callback': None, 'control':None, 'state':None, 'cost_name': None, 'params': None, 'error_params': None}):
+        ''' Call chosen optimization routine with user-selected cost function and parameters '''
+
+        ''' Load any non-passed settings from the GUI '''
+        gui_settings = self.get_settings_from_gui()
+        print(settings)
+        for s in settings:
+            if settings[s] is None:
+                settings[s] = gui_settings[s]
+
+        settings['cost'] = getattr(settings['control'], settings['cost_name'])
+        optimizer, index = settings['control'].attach_optimizer(settings['state'], settings['cost'])
+        settings['control'].optimizers[index]['status'] = 'Servoing'
+        t = datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
+        row = self.parent.historyPanel.add_event(t, settings['cost_name'], 'PID', 'Servoing', optimizer)
+        func = optimizer.PID
+
+        if settings['state'] == {}:
             log.warn('Please select at least one Input node for optimization.')
         else:
-            log.info('Started optimization of %s experiment using %s algorithm.'%(cost_name, 'PID'))
-            self._run_thread(self.start_optimizer, args=(func, state, cost, params, error_params, control, optimizer, index, row, t, cost_name, 'PID'), stoppable=False)
+            log.info('Started optimization of %s experiment using %s algorithm.'%(settings['cost_name'], 'PID'))
+            self._run_thread(self.start_optimizer, args=(func, settings, optimizer, index, row, t, 'PID'), stoppable=False)
 
-    def start_optimizer(self, func, state, cost, params, error_params, control, optimizer, index, row, t, cost_name, algorithm_name):
-        func(state, cost, params, error_params)
+    def start_optimizer(self, func, settings, optimizer, index, row, t, algorithm_name):
+        func(settings['state'], settings['cost'], settings['params'], settings['error_params'], callback = settings['callback'])
         log.info('Optimization complete!')
-        control.optimizers[index]['status'] = 'Done'
-        optimizer.log(t.replace(':','') + ' - ' + cost_name + ' - ' + algorithm_name)
+        settings['control'].optimizers[index]['status'] = 'Done'
+        optimizer.log(t.replace(':','') + ' - ' + settings['cost_name'] + ' - ' + algorithm_name)
 
     def stop_optimizer(self):
         control = self.parent.treeWidget.get_selected_control()
