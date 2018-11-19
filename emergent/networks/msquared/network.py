3a5ee5d08201025a3461c8c6ea7cda500648f8e3
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-11-19 13:52:01 -0700

    Add PZT device

--- emergent/networks/msquared/network.py
+++ emergent/networks/msquared/network.py
@@ -1,9 +1,13 @@
 from emergent.networks.msquared.controls.lock import Lock
 from emergent.devices.SolsTiS import SolsTiS
 from emergent.devices.bristol import Wavemeter
+from emergent.devices.PZT import PZT
 import sys
 
 lock = Lock('lattice')
 params = {'server_ip':'192.168.1.207', 'client_ip':'192.168.1.100', 'port':39933}
 s = SolsTiS(params, parent=lock)
+
+params = {'devid': '440010635'}
+pzt = PZT(params, parent=lock)
 wm = Wavemeter(addr='TCPIP::10.199.199.1::23::SOCKET', parent=lock)
