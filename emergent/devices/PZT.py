d6a1e056a199a9a6fb0df8e57b78578baf7ab1a2
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-11-19 13:50:38 -0700

    Add toggle_lock method

--- emergent/devices/PZT.py
+++ emergent/devices/PZT.py
@@ -6,7 +6,9 @@ class PZT(Device):
         super().__init__(name=name, parent = parent)
         self.labjack = LabJack(devid=params['devid'])
         self.add_input('voltage')
-
+        self.options['Toggle lock'] = self.toggle_lock
+        self.lock(0)
+        
     def _connect(self):
         return 1
 
@@ -14,13 +16,17 @@ class PZT(Device):
         self.labjack.AOut(0,state['voltage'])
 
     def lock(self, state):
-        return
+        self.lock_state = state
+        self.labjack.DOut(6,1-state)
+
+    def toggle_lock(self):
+        self.lock(1-self.lock_state)
 
     def get_cavity_transmission(self):
         return self.labjack.AIn(0)
 
     def get_servo_output(self):
         return self.labjack.AIn(1)
-        
+
 if __name__ == '__main__':
     params = {'devid':'440010635'}
