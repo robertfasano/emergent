c735604d32a5c50ebec20ce7093400693fe964d5
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-11-27 12:26:58 -0700

    Speed up probe_pulse

--- emergent/networks/gmot/controls/mot.py
+++ emergent/networks/gmot/controls/mot.py
@@ -7,13 +7,14 @@ import numpy as np
 from emergent.archetypes.parallel import ProcessHandler
 from emergent.devices.labjackT7 import LabJack
 import matplotlib.pyplot as plt
+from emergent.utility import Timer
 
 class MOT(Control):
     def __init__(self, name, parent = None, path='.'):
         super().__init__(name, parent = parent, path=path)
         self.process_manager = ProcessHandler()
         self.trigger_labjack = LabJack(devid='440010734', name = 'trigger')
-
+        self.timer = Timer()
     def add_labjack(self, labjack):
         self.labjack = labjack
         self.labjack.prepare_streamburst(channel=0)
@@ -46,35 +47,55 @@ class MOT(Control):
             print('signal:',signal)
 
     @experiment
-    def probe_pulse(self, state, params = {'loading time': 1, 'probe time': 0.1, 'trigger delay': 0.01}):
+    def probe_pulse(self, state, params = {'stream step': 0.001, 'probe intensity': 0.3, 'integration time': 0.005, 'loading time': 1, 'probe time': 0.1, 'trigger delay': 0.001}):
         ''' Queue triggered stream-out on intensity servo channels '''
+        if 'servo' in state:
+            state['servo']['V0'] = 0
         self.actuate(state)
+
         cycle_time = params['loading time'] + params['probe time']
         servo = self.children['servo']
+        t = np.linspace(0,cycle_time, int(1e4))
+        ''' Minimum number of samples is the cycle time divided by the required precision '''
+        precision = params['stream step']
+        samples = int(cycle_time / precision)
 
+        self.timer.log('Preparing probe stream')
         probe_labjack = servo.labjack[0]
-        probe_stream = np.array([[0,0], [params['loading time'], self.state['servo']['V0']]])
+        y = np.zeros((len(t),2))
+        y[t>params['loading time'], 0] = params['probe intensity']
         probe_labjack.prepare_stream_out(trigger=0)
-        sequence, scanRate = probe_labjack.resample(probe_stream, cycle_time)
-        probe_labjack.stream_out([0], sequence, scanRate, loop=0)
+        sequence, scanRate = probe_labjack.resample(y, cycle_time, max_samples = samples)
 
+        self.timer.log('Writing probe stream')
+        probe_labjack.stream_out([0,1], sequence, scanRate, loop=0)
+
+        self.timer.log('Preparing trap stream')
         trap_labjack = servo.labjack[1]
-        t = np.linspace(0,cycle_time, 1000)
         y = np.zeros((len(t),2))
         y[t<params['loading time'], 0] = self.state['servo']['V2']
         y[t<params['loading time'], 1] = self.state['servo']['V3']
         trap_labjack.prepare_stream_out(trigger=0)
-        sequence, scanRate = trap_labjack.resample(y, cycle_time)
+        sequence, scanRate = trap_labjack.resample(y, cycle_time, max_samples = samples)
+        self.timer.log('Writing trap stream')
         trap_labjack.stream_out([0,1], sequence, scanRate, loop=0)
 
         ''' Queue triggered stream-in on self.labjack '''
+        self.timer.log('Preparing input stream')
         if self.labjack.stream_mode is not 'in-triggered':
             self.labjack.prepare_streamburst(0, trigger=0)
         self.process_manager._run_thread(self.probe_trigger, args=(params['trigger delay'],), stoppable=False)
+        self.timer.log('Running input stream')
         data = self.labjack.streamburst(cycle_time)
+        self.timer.log('Finished streaming')
+        time_per_point = cycle_time/len(data)
+        probe_point = int(params['loading time']/time_per_point)
+        integration_points = int(params['integration time']/time_per_point)
+
+        value = np.array(data)[probe_point:probe_point+integration_points].sum()
         print(np.max(data))
-        plt.plot(data)
-        self.actuate(state)
+        # plt.plot(data)
+        # plt.show()
         return -np.max(data)
 
     @experiment
@@ -101,10 +122,9 @@ class MOT(Control):
     def probe_switch(self, state, params = {'loading time': 1, 'probe time': 0.1, 'trigger delay': 0.01}):
         ''' Stream on the digital channels to switch RF switches '''
 
-    def probe_trigger(self, delay = 1):
-        time.sleep(delay)
+    def probe_trigger(self, trigger_delay = 0.001):
         for i in range(10):
-            time.sleep(0.001)
+            time.sleep(trigger_delay)
             self.trigger_labjack.DOut(4, i%2)
 
     def wave(self, frequency=2):
