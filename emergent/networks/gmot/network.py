bcba0e2146be5549139f427c8d4eb724213eff4f
rjf2 <robert.fasano@nist.gov>
2018-12-18 14:28:16 -0700

    Move node instantiation to initialize method

--- emergent/networks/gmot/network.py
+++ emergent/networks/gmot/network.py
@@ -4,20 +4,26 @@ from emergent.things import LabJack, PicoAmp, NetHubs, Novatech
 from emergent.networks.gmot.things import CurrentDriver, IntensityServo
 from __main__ import *
 
-''' Define autoAlign '''
-labjack_cooling = LabJack(thingid='470016934', name='cooling')
-cooling = AutoAlign(name='cooling', labjack=labjack_cooling, path='networks/%s'%sys.argv[1])
-mems_cooling = PicoAmp('MEMS', labjack_cooling, parent=cooling)
 
-labjack_slowing = LabJack(thingid='470017899', name='slowing')
-slowing = AutoAlign(name='slowing', labjack=labjack_slowing, path='networks/%s'%sys.argv[1])
-mems_slowing = PicoAmp('MEMS', labjack_slowing, parent=slowing)
 
-''' Define MOT hub hub '''
-mot = MOT(name='MOT', path='networks/%s'%sys.argv[1])
-loader = Loader(name='loader', parent=mot)
-# feedthrough = NetHubs('feedthrough', 'COM11', parent = mot)
-novatech = Novatech('novatech', 'COM4', parent = mot)
-servo = IntensityServo('servo', '470016973', parent = mot)
-labjack_coils = LabJack(thingid='440010680', name = 'labjack')
-coils = CurrentDriver('coils', 'COM13', 'COM18', parent = mot,labjack = labjack_coils)
+def initialize(network):
+    ''' Define autoAlign '''
+    labjack_cooling = LabJack(thingid='470016934', name='cooling')
+    cooling = AutoAlign(name='cooling', labjack=labjack_cooling, path='networks/%s'%sys.argv[1])
+    mems_cooling = PicoAmp('MEMS', labjack_cooling, parent=cooling)
+    network.addHub(cooling)
+
+    labjack_slowing = LabJack(thingid='470017899', name='slowing')
+    slowing = AutoAlign(name='slowing', labjack=labjack_slowing, path='networks/%s'%sys.argv[1])
+    mems_slowing = PicoAmp('MEMS', labjack_slowing, parent=slowing)
+    network.addHub(slowing)
+
+    ''' Define MOT hub '''
+    mot = MOT(name='MOT', path='networks/%s'%sys.argv[1])
+    loader = Loader(name='loader', parent=mot)
+    # feedthrough = NetHubs('feedthrough', 'COM11', parent = mot)
+    novatech = Novatech('novatech', 'COM4', parent = mot)
+    servo = IntensityServo('servo', '470016973', parent = mot)
+    labjack_coils = LabJack(thingid='440010680', name = 'labjack')
+    coils = CurrentDriver('coils', 'COM13', 'COM18', parent = mot,labjack = labjack_coils)
+    network.addHub(mot)
--- emergent/networks/msquared/network.py
+++ emergent/networks/msquared/network.py
@@ -2,10 +2,12 @@ from emergent.networks.msquared.hubs.lock import Lock
 from emergent.networks.msquared.things import PZT, SolsTiS, Wavemeter
 import sys
 
-lock = Lock('lattice')
-params = {'server_ip':'192.168.1.207', 'client_ip':'192.168.1.100', 'port':39933}
-s = SolsTiS(params, parent=lock)
+def initialize(network):
+    lock = Lock('lattice')
+    params = {'server_ip':'192.168.1.207', 'client_ip':'192.168.1.100', 'port':39933}
+    s = SolsTiS(params, parent=lock)
 
-params = {'thingid': '440010635'}
-pzt = PZT(params, parent=lock)
-wm = Wavemeter(addr='TCPIP::10.199.199.1::23::SOCKET', parent=lock)
+    params = {'thingid': '440010635'}
+    pzt = PZT(params, parent=lock)
+    wm = Wavemeter(addr='TCPIP::10.199.199.1::23::SOCKET', parent=lock)
+    network.addHub(lock)
