bdb4534d5f6fbd7962374403249b3975cab690a8
Robert Fasano <robert.j.fasano@colorado.edu>
2018-12-18 06:23:45 -0700

    Add stochastic optimization features

--- emergent/optimizers/adam.py
+++ emergent/optimizers/adam.py
@@ -1,5 +1,6 @@
 from utility import Parameter, algorithm
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Adam():
     def __init__(self):
@@ -31,15 +32,26 @@ class Adam():
                                             min = 0.9,
                                             max = 0.999,
                                             description = 'Exponential decay rate for second-moment estimates')
-
+        self.params['noise'] = Parameter(name= 'noise',
+                                            value = 0,
+                                            min = 0,
+                                            max = 0.1,
+                                            description = 'Noise injection for stochastic optimization')
+        self.params['restarts'] = Parameter(name= 'restarts',
+                                            value = 3,
+                                            min = 0,
+                                            max = 10,
+                                            description = 'Number of restarts allowed after divergence')
     @algorithm
     def run(self, state):
         arr, bounds = self.sampler.prepare(state)
         costs = [self.sampler._cost(arr)]
+        points = [arr]
         m = np.zeros(len(arr))
         v = np.zeros(len(arr))
         epsilon = 1e-8
         gradients = []
+        restarts = 0
         while True:
             ''' compute gradient '''
             g = self.sampler.estimate_gradient(arr, self.params['Dither'].value)
@@ -47,8 +59,17 @@ class Adam():
 
             gmag = np.dot(g,g)
             max_gmag = np.dot(np.max(gradients), np.max(gradients))
-            if gmag/max_gmag < self.params['Tolerance'].value and costs[-1] < np.max(costs):
-                break
+            if gmag/max_gmag < self.params['Tolerance'].value:
+                if costs[-1] < costs[0]:
+                    break
+                else:
+                    if restarts < self.params['restarts'].value:
+                        points.append(points[np.argmin(costs)])
+                        costs.append(self.sampler._cost(points[-1]))
+                        restarts += 1
+                        continue
+                    else:
+                        return
             m = self.params['beta_1'].value*m+(1-self.params['beta_1'].value)*g
             v = self.params['beta_2'].value*v + (1-self.params['beta_2'].value)*g**2
 
@@ -56,7 +77,8 @@ class Adam():
             vhat = v/(1-self.params['beta_2'].value)
 
             ''' move along gradient '''
-            arr = arr - self.params['Learning rate'].value*mhat/(np.sqrt(vhat)+epsilon)
+            arr = arr - self.params['Learning rate'].value*mhat/(np.sqrt(vhat)+epsilon)+np.random.normal(0, self.params['noise'].value, size=arr.shape)
+            points.append(arr)
             costs.append(self.sampler._cost(arr))
 
 
@@ -66,3 +88,6 @@ class Adam():
     def set_params(self, params):
         for p in params:
             self.params[p].value = params[p]
+
+    def plot(self):
+        return plt.figure()
