a0582ccd99d90725cf4db0d78242779ab09b0d35
Robert Fasano <robert.j.fasano@colorado.edu>
2018-12-19 21:10:42 -0700

    Create shortcut and batch file in setup.py

--- setup.py
+++ setup.py
@@ -10,3 +10,19 @@ setup(
     license='MIT',
     long_description=open('README.md').read(),
 )
+
+''' Prepare batch file and shortcut '''
+import sys
+scripts_dir = sys.executable.split('python')[0]+'Scripts/'
+with open('emergent/run.cmd', 'w') as file:
+    file.write('@ECHO OFF\ncall %sactivate.bat\ncall python launcher.py'%scripts_dir)
+
+import win32com.client
+import os
+ws = win32com.client.Dispatch("wscript.shell")
+shortcut = ws.CreateShortcut('emergent/EMERGENT.lnk')
+shortcut.TargetPath = 'C:/Windows/System32/cmd.exe'
+shortcut.Arguments = '/k %s/emergent/run.cmd'%os.getcwd()
+shortcut.IconLocation = '%s/emergent/gui/media/icon.ico'%os.getcwd()
+
+shortcut.Save()
