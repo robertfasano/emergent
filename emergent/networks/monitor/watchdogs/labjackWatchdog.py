054a68399e5426a358f88684f584197c2094c152
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-12-21 13:27:14 -0700

    Use default Watchdog class

--- emergent/networks/monitor/watchdogs/labjackWatchdog.py
+++
@@ -1,6 +0,0 @@
-from emergent.modules import Watchdog
-
-class LabJackWatchdog(Watchdog):
-    def __init__(self, parent, experiment, threshold, name = 'watchdog'):
-        super().__init__(parent, experiment, name = name)
-        self.threshold = threshold
