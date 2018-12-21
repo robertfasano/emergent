965fd287b93aca4b402d96a39ab25d045087e47c
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-12-21 14:59:10 -0700

    Add threshold

--- emergent/networks/example/watchdogs/testWatchdog.py
+++ emergent/networks/example/watchdogs/testWatchdog.py
@@ -1,9 +1,8 @@
 from emergent.modules import Watchdog
 
 class TestWatchdog(Watchdog):
-    def __init__(self, parent, experiment, name = 'watchdog'):
-        super().__init__(parent, experiment, name = name)
-        self.threshold = 0.5
+    def __init__(self, parent, experiment, threshold = 0.5, name = 'watchdog'):
+        super().__init__(parent, experiment, threshold, name = name)
 
     def react(self):
         self.reoptimize(self.parent.state, 'transmitted_power')
