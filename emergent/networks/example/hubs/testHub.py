72d680fca9e1338d19ea82ee569bce78cbf008a3
Robbie Fasano <robert.j.fasano@colorado.edu>
2018-12-21 11:58:35 -0700

    Move watchdog to standalone folder

--- emergent/networks/example/hubs/testHub.py
+++ emergent/networks/example/hubs/testHub.py
@@ -7,33 +7,9 @@ import numpy as np
 import socket
 import importlib
 
-class TestWatchdog(Watchdog):
-    def __init__(self, parent, name = 'watchdog'):
-        super().__init__(parent, name)
-        self.threshold = 0.5
-
-    def measure(self):
-        ''' Measures power at the current state. This is an example of a signal that a Watchdog can monitor - if the Watchdog
-            called the original @experiment, we would have recursion issues! '''
-        x=self.parent.state['MEMS']['X']
-        y=self.parent.state['MEMS']['Y']
-        params = {'sigma_x': 0.3, 'sigma_y': 0.8, 'x0': 0.3, 'y0': 0.6, 'noise':0}
-        x0 = params['x0']
-        y0 = params['y0']
-        sigma_x = params['sigma_x']
-        sigma_y = params['sigma_y']
-        power =  np.exp(-(x-x0)**2/sigma_x**2)*np.exp(-(y-y0)**2/sigma_y**2) + np.random.normal(0, params['noise'])
-
-        return power
-
-    def react(self):
-        self.reoptimize(self.parent.state, 'transmitted_power')
-
-
 class TestHub(Hub):
         def __init__(self, name, addr=None, network=None):
                 super().__init__(name, addr, network = network)
-                self.watchdogs['watchdog'] = TestWatchdog(self, 'fiber power')
 
         def cost_coupled(self, state, params={}):
             return self.cost_uncoupled(state, theta=30*np.pi/180)
--- emergent/networks/example/network.py
+++ emergent/networks/example/network.py
@@ -1,10 +1,13 @@
 from emergent.networks.example.hubs.testHub import TestHub
 from emergent.networks.example.things.testThing import TestThing
+from emergent.networks.example.watchdogs.testWatchdog import TestWatchdog
 import sys
 import socket
 #
 def initialize(network):
     autoAlign = TestHub('autoAlign', network = network)
+    autoAlign.watchdogs['watchdog'] = TestWatchdog(autoAlign, 'fiber power')
+
     MEMS = TestThing('MEMS', parent=autoAlign, inputs = ['X', 'Y'])
     otherHub = TestHub('otherHub', addr = '127.0.0.1', network = network)
     otherThing = TestThing('otherThing', parent=otherHub, inputs = ['X', 'Y'])
