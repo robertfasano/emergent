611431447b94a9f14add62101ccf80bd9106e3c5
Robert Fasano <robert.j.fasano@colorado.edu>
2018-12-23 18:47:30 -0700

    Add Client

--- emergent/modules/__init__.py
+++ emergent/modules/__init__.py
@@ -2,5 +2,6 @@ from emergent.modules.sampler import Sampler
 from emergent.modules.state import State
 from emergent.modules.parallel import ProcessHandler
 from emergent.modules.node import Input, Thing, Hub
+from emergent.modules.client import Client
 from emergent.modules.network import Network
 from emergent.modules.watchdog import Watchdog
