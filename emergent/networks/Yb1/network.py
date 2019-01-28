521466d225d7244e3e2d2e933165f37f87178d23
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-01-28 15:18:39 -0700

    Refactor monitor implementation

--- emergent/networks/Yb1/network.py
+++ emergent/networks/Yb1/network.py
@@ -2,7 +2,7 @@ from emergent.networks.Yb1.hubs.lattice import Lattice
 from emergent.networks.Yb1.hubs.waveform import Ramp
 from emergent.modules import Thing
 from emergent.things.labjack import LabJack
-
+from emergent.networks.monitor import network as monitor
 from __main__ import *
 
 # ''' Define Lattice '''
@@ -14,7 +14,16 @@ from __main__ import *
 
 
 def initialize(network):
-    lattice = Lattice('lattice', network = network)
+    # lattice = Lattice('lattice', network = network)
+    params = {'daqs': [{'name': 'labjack', 'params':{'devid': '440010742'}}],
+              'watchdogs': {
+                        'Lattice intensity': {'threshold': 0.1, 'channel': 'A0'},
+                        'Lattice cavity': {'threshold': 0.1, 'channel': 'A1'},
+                        'Blue Yb1 fiber': {'threshold': 3, 'channel': 'A2'},
+                        'Blue error signal': {'threshold': 0.1, 'channel': 'A3'},
+                        }
+                     }
+    monitor.initialize(network, params = params)
 
-    for hub in [monitor]:
-        network.addHub(lattice)
+    for hub in []:
+        network.addHub(hub)
