dbe003f72b80141448c0e6d5d36b5e5c238ea2f5
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-01-30 13:21:16 -0700

    Set up photoassociation experiment

--- emergent/networks/Yb1/network.py
+++ emergent/networks/Yb1/network.py
@@ -1,29 +1,25 @@
-from emergent.networks.Yb1.hubs.lattice import Lattice
-from emergent.networks.Yb1.hubs.waveform import Ramp
-from emergent.modules import Thing
+from emergent.networks.Yb1.hubs.PA import Photoassociation
+from emergent.networks.Yb1.things.e4430b import E4430B
 from emergent.things.labjack import LabJack
 from emergent.networks.monitor import network as monitor
 from __main__ import *
 
-# ''' Define Lattice '''
-# lattice = Lattice(name='lattice', path='networks/%s'%sys.argv[1])
-# lattice.ljIN = LabJack(devid='470016973', name='labjack')
-# lattice.ljIN.prepare_streamburst(channel=0, trigger = 0, max_samples = lattice.max_samples)
-#
-# lattice.ljOUT = LabJack(devid='470016970', name='labjack')
-
-
 def initialize(network):
-    # lattice = Lattice('lattice', network = network)
-    params = {'daqs': [{'name': 'labjack', 'params':{'devid': '440010742'}}],
-              'watchdogs': {
-                        'Lattice intensity': {'threshold': 0.1, 'channel': 'A0'},
-                        'Lattice cavity': {'threshold': 0.1, 'channel': 'A1'},
-                        'Blue Yb1 fiber': {'threshold': 3, 'channel': 'A2'},
-                        'Blue error signal': {'threshold': 0.1, 'channel': 'A3'},
-                        }
-                     }
-    monitor.initialize(network, params = params)
+    pa = Photoassociation(name = 'photoassociation', network = network)
+    pa.labjack = LabJack(name = 'labjack', params = {'devid': '440010742'}, parent = pa)
+    pa.labjack.add_input('TDAC6')
 
-    for hub in []:
+    synth = E4430B('synthesizer', parent = pa)
+
+    # params = {'daqs': [{'name': 'labjack', 'params':{'devid': '440010742'}}],
+    #           'watchdogs': {
+    #                     'Lattice intensity': {'threshold': 0.1, 'channel': 'A0'},
+    #                     'Lattice cavity': {'threshold': 0.1, 'channel': 'A1'},
+    #                     'Blue Yb1 fiber': {'threshold': 3, 'channel': 'A2'},
+    #                     'Blue error signal': {'threshold': 0.1, 'channel': 'A3'},
+    #                     }
+    #                  }
+    # monitor.initialize(network, params = params)
+
+    for hub in [pa]:
         network.addHub(hub)
