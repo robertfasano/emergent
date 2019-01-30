9edb6896daeba126a9c78c81f70290bdc31da717
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-01-30 13:20:35 -0700

    Initial commit

---
+++ emergent/networks/Yb1/things/e4430b.py
@@ -0,0 +1,27 @@
+import serial
+import sys
+from emergent.protocols.serial import Serial
+from emergent.modules import Thing
+import logging as log
+
+class E4430B(Thing):
+    def __init__(self, name, params = {}, parent = None):
+        super().__init__(name=name, params = params, parent = parent)
+        self._connected = self._connect()
+        self.add_input('frequency')
+        self.add_input('amplitude')
+
+    def _connect(self):
+        import visa
+        rm = visa.ResourceManager()
+        self.client = rm.open_resource('GPIB0::29::INSTR')
+        return 1
+
+    def _actuate(self, state):
+        ''' set frequency '''
+        if 'frequency' in state:
+            self.client.write('FREQuency %f MHz'%state['frequency'])
+
+        ''' set amplitude '''
+        if 'amplitude' in state:
+            self.client.write('POWer %f dBm'%state['amplitude'])
