05d0a97455c057008685a116db95cdfcec8df15b
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-02-05 11:02:38 -0700

    Fix param refactoring

--- emergent/networks/autoAlign/network.py
+++ emergent/networks/autoAlign/network.py
@@ -2,10 +2,10 @@ from emergent.networks.autoAlign.hubs import AutoAlign
 from emergent.networks.autoAlign.things import PicoAmp
 from emergent.things import LabJack
 
-def initialize(network, params = {}):
+def initialize(network, params = {'autoAlign': {'name': '', 'params': {'MEMS':{'params': {'devid': ''}}}}}):
     network.add_params(params)
-
-    labjack = LabJack(name='labjack', params = {'devid': '470017899'})
+    devid = params['autoAlign']['params']['MEMS']['params']['devid']
+    labjack = LabJack(name='labjack', params = {'devid': devid})
     hub = AutoAlign(name='autoAlign', labjack=labjack, network = network)
     thing = PicoAmp('MEMS', parent=hub, params = {'labjack': labjack, 'type': 'digital'})
 
