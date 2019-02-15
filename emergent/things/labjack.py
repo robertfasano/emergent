50231873798706ff1fd8377ecc898c5e94654614
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-02-15 10:34:35 -0700

    Refactor streaming methods to share code between different stream types

--- emergent/things/labjack.py
+++ emergent/things/labjack.py
@@ -91,6 +91,11 @@ class LabJack(Thing):
                 ljm.eStreamStop(self.handle)
             except:
                 pass
+
+
+            ''' Set default parameters '''
+            aNames = ['AIN_ALL_NEGATIVE_CH', 'AIN_ALL_RANGE']
+            aValues = [ljm.constants.GND, 10.0]
             return 1
 
         except Exception as e:
@@ -277,48 +282,6 @@ class LabJack(Thing):
 
         return y
 
-
-
-    ''' Streaming methods '''
-    def prepare_digital_stream(self, channels):
-        # prepare inhibit array
-        inhibit = ''
-        for i in range(23):
-            if 23-i-1 in channels:
-                inhibit += '0'
-            else:
-                inhibit += '1'
-        inhibit = int(inhibit, 2)
-        self._command('DIO_INHIBIT', inhibit)
-
-        # set direction
-        bitmask = 0
-        for ch in channels:
-            bitmask = bitmask | 1 << ch
-        self._command('DIO_DIRECTION', bitmask)
-
-    def state_bitmask(self, channels, states):
-        bitmask = 0
-        for i in range(len(channels)):
-            bitmask = bitmask | (states[i] << channels[i])
-        return bitmask
-
-    def digital_stream(self, channels = [1,3], period = 1e-3):
-        self.prepare_digital_stream(channels)
-        t = np.linspace(0,1,100)
-        Y = np.zeros((len(t),2))
-
-        Y[t<0.5, 0] = 1
-        Y[t<0.7, 1] = 1
-
-        ''' convert multidimensional array to array of bitmasks '''
-        y = self.array_to_bitmask(Y, channels)
-
-        self.prepare_stream_out()
-        y, f = self.resample(np.atleast_2d(y).T, period)
-
-        self.stream_out(['FIO_STATE'], y.astype(int), f, loop = 1)
-
     def array_to_bitmask(self, arr, channels):
         ''' Convert multidimensional array with one column for each channel to an array of bitmasks. '''
         y = np.zeros(len(arr))
@@ -330,34 +293,7 @@ class LabJack(Thing):
             y[i] = bitmask
         return y
 
-    def prepare_streamburst(self, channel, max_samples = 2**13-1, trigger = None):
-        ''' Sets up the LabJack for burst input streaming on a target channel. '''
-        self.aScanList = ljm.namesToAddresses(1, ['AIN%i'%channel])[0]  # Scan list addresses for streamBurst
-
-        if trigger is None:
-            self._command("STREAM_TRIGGER_INDEX", 0) # disable triggered stream
-            aNames = ['STREAM_TRIGGER_INDEX']
-            aValues = [0]
-        else:
-            channel = 'DIO%i'%trigger
-            aNames = ["%s_EF_ENABLE"%channel, "%s_EF_INDEX"%channel,
-                      "%s_EF_OPTIONS"%channel, "%s_EF_VALUE_A"%channel,
-                      "%s_EF_ENABLE"%channel, 'STREAM_TRIGGER_INDEX']
-            aValues = [0, 3, 0, 2, 1, 2000+trigger]
-            # self._command('STREAM_TRIGGER_INDEX', 2000+trigger)
-            ljm.writeLibraryConfigS('LJM_STREAM_RECEIVE_TIMEOUT_MS',0)  #disable timeout
-        if self.deviceType == ljm.constants.dtT7:
-            # self._command("STREAM_CLOCK_SOURCE", 0)  # enable internal clock
-            aNames.append('STREAM_CLOCK_SOURCE')   # enable internal clock
-            aValues.append(0)
-        aNames.extend(["AIN_ALL_NEGATIVE_CH", "AIN0_RANGE", "AIN1_RANGE",
-                  "STREAM_SETTLING_US", "STREAM_RESOLUTION_INDEX", 'STREAM_BUFFER_SIZE_BYTES'])
-        n = np.ceil(np.log10(2*(1+max_samples))/np.log10(2))
-        buffer_size = 2**n
-        aValues.extend([ljm.constants.GND, 10.0, 10.0, 0, 0, buffer_size])
-        self._write_array(aNames, aValues)
-        self.stream_mode = 'in-triggered'
-
+    ''' Streaming functions '''
     def streamburst(self, duration, max_samples=2**13-1, operation = None):
         ''' Performs a burst stream and optionally performs a numpy array operation
             on the result.
@@ -367,22 +303,7 @@ class LabJack(Thing):
                 automatically adjusted to avoid overfilling the buffer.
 
                 '''
-        if self.deviceType == ljm.constants.dtT7:
-            max_speed = 100000
-        elif self.deviceType == ljm.constants.dtT4:
-            max_speed = 40000
-
-        cutoff = max_samples / max_speed
-        if duration >= cutoff:
-            samples = max_samples
-            scanRate = int(samples/duration)
-            # print('Speed limited; samples = %f, speed = %f'%(samples, speed))
-        else:
-            scanRate = max_speed
-            samples = int(duration*scanRate)
-        # scanRate = np.min([max_samples / duration, max_speed])
-        # samples = max_samples
-
+        scanRate, samples = self.get_scanrate(duration, max_samples)
         scanRate, aData = ljm.streamBurst(self.handle, 1, self.aScanList, scanRate, samples)
 
         if operation is None:
@@ -390,54 +311,8 @@ class LabJack(Thing):
         else:
             return getattr(np, operation)(aData)
 
-    @queue
-    def stream_stop(self):
-        ljm.eStreamStop(self.handle)
-
-    def prepare_stream_out(self, trigger = None):
-        ''' Prepares an output stream.
-
-            Args:
-                trigger (int): if not None, set an FIO channel to trigger on
-
-            Note:
-                The maximum buffer size of the LabJack T7 is 2^15=32768 bytes,
-                so up to 2^14=16384 samples can be held. At the default stream
-                rate of 100 kS/s, this corresponds to 163.84 ms of data. If we
-                want to stream for longer than that, we need to repeatedly write
-                half a buffer's worth of data each time the buffer is half full.
-        '''
-        try:
-            ''' Stop streaming if currently running '''
-            ljm.eStreamStop(self.handle)
-        except:
-            pass
-
-        if trigger is None:
-            self._command("STREAM_TRIGGER_INDEX", 0)        # Ensure triggered stream is disabled.
-            aNames = ['STREAM_TRIGGER_INDEX']
-            aValues = [0]
-        else:
-            channel = 'DIO%i'%trigger
-            aNames = ["%s_EF_ENABLE"%channel, "%s_EF_INDEX"%channel,
-                      "%s_EF_OPTIONS"%channel, "%s_EF_VALUE_A"%channel,
-                      "%s_EF_ENABLE"%channel, 'STREAM_TRIGGER_INDEX']
-            aValues = [0, 3, 0, 2, 1, 2000+trigger]
-
-
-        aNames.extend(["STREAM_SETTLING_US", "STREAM_RESOLUTION_INDEX"])
-        aValues.extend([0, 0])
-        if self.deviceType == ljm.constants.dtT7:
-            aNames.append('STREAM_CLOCK_SOURCE')    # Enabling internally-clocked stream.
-            aValues.append(0)
-        self._write_array(aNames, aValues)
-        if trigger is not None:
-            self.stream_mode = 'out-triggered'
-        else:
-            self.stream_mode = 'out'
-
     def stream_out(self, channels, data, scanRate, loop = 0):
-        ''' Streams data at a given scan rate. Currently only supports analog channels.
+        ''' Streams data at a given scan rate.
 
             Args:
                 channels (list): Output channels to stream on, e.g. ['DAC0', 'DAC1']
@@ -453,7 +328,6 @@ class LabJack(Thing):
             pass
         n = np.ceil(np.log10(2*(1+len(data)))/np.log10(2))
         buffer_size = 2**n
-        aScanList = []
         for i in range(len(channels)):
             aNames =["STREAM_OUT%i_TARGET"%i,
                            "STREAM_OUT%i_BUFFER_SIZE"%i,
@@ -478,59 +352,109 @@ class LabJack(Thing):
                            "STREAM_OUT%i_SET_LOOP"%i]
             aValues = [loop*len(data), 1]
             self._write_array(aNames, aValues)
-            aScanList.append(4800+i)
+            self.aScanList.append(4800+i)
 
-        scanRate = ljm.eStreamStart(self.handle, 1,len(aScanList), aScanList, scanRate)
-        # log.info("\nStream started with a scan rate of %0.0f Hz." % scanRate)
+        scanRate = ljm.eStreamStart(self.handle, 1, len(self.aScanList), self.aScanList, scanRate)
 
-    def sequence2stream(self, sequence, period, max_samples = None, channels = 1):
-        ''' Converts a sequence to a stream. The LabJack has two limitations:
-            a maximum stream rate of 100 kS/s and a maximum sample count of
-            2^13-1. This method computes a cutoff period based on these two
-            restrictions; above the cutoff, the speed will be lowered to accommodate
-            longer sequences with equal numbers of samples, while below the cutoff,
-            the number of samples will be lowered while the speed is kept at max.
+    @queue
+    def stream_stop(self):
+        ljm.eStreamStop(self.handle)
+
+    ''' Stream preparation functions '''
+    def prepare_digital_stream(self, channels):
+        # prepare inhibit array
+        inhibit = ''
+        for i in range(23):
+            if 23-i-1 in channels:
+                inhibit += '0'
+            else:
+                inhibit += '1'
+        inhibit = int(inhibit, 2)
+        self._command('DIO_INHIBIT', inhibit)
+
+        # set direction
+        bitmask = 0
+        for ch in channels:
+            bitmask = bitmask | 1 << ch
+        self._command('DIO_DIRECTION', bitmask)
+
+    def prepare_stream(self):
+        try:
+            ''' Stop streaming if currently running '''
+            ljm.eStreamStop(self.handle)
+        except:
+            pass
+        self.aScanList = []
+        aNames = ['STREAM_SETTLING_US', 'STREAM_RESOLUTION_INDEX', 'STREAM_CLOCK_SOURCE']
+        aValues = [0, 0, 0]
+        self._write_array(aNames, aValues)
+
+    def prepare_stream_trigger(self, trigger):
+        if trigger is None:
+            self._command("STREAM_TRIGGER_INDEX", 0) # disable triggered stream
+            aNames = ['STREAM_TRIGGER_INDEX']
+            aValues = [0]
+        else:
+            channel = 'DIO%i'%trigger
+            aNames = ["%s_EF_ENABLE"%channel, "%s_EF_INDEX"%channel,
+                      "%s_EF_OPTIONS"%channel, "%s_EF_VALUE_A"%channel,
+                      "%s_EF_ENABLE"%channel, 'STREAM_TRIGGER_INDEX']
+            aValues = [0, 3, 0, 2, 1, 2000+trigger]
+            ljm.writeLibraryConfigS('LJM_STREAM_RECEIVE_TIMEOUT_MS',0)  #disable timeout
+        self._write_array(aNames, aValues)
+
+    def prepare_streamburst(self, channel, max_samples = 2**13-1, trigger = None):
+        ''' Sets up the LabJack for burst input streaming on a target channel. '''
+        self.prepare_stream()
+        self.prepare_stream_trigger(trigger)
+        self.aScanList.extend(ljm.namesToAddresses(1, ['AIN%i'%channel])[0])  # Scan list addresses for streamBurst
+
+        ''' Prepare buffer '''
+        n = np.ceil(np.log10(2*(1+max_samples))/np.log10(2))
+        buffer_size = 2**n
+        self._command('STREAM_BUFFER_SIZE_BYTES', buffer_size)
+        self.stream_mode = 'in-triggered'
+
+    def prepare_stream_out(self, trigger = None, channels = []):
+        ''' Prepares an output stream.
 
             Args:
-                sequence (list): A list of tuples describing all changes in a setpoint; e.g. [(0,0), (0.5,1)] describes a setpoint which is switched from 0 to 1 at t=0.5.
-                period (float): The total sequence duration.
-                channels (int): Number of channels which will be simultaneously streamed; the maximum sampling rate is reduced by this factor.
+                trigger (int): if not None, set an FIO channel to trigger on
 
-            Returns:
-                stream (array): A list of points which will be output at the calculated sampling rate.
-                speed (float): Stream rate in samples/second.
+            Note:
+                The maximum buffer size of the LabJack T7 is 2^15=32768 bytes,
+                so up to 2^14=16384 samples can be held. At the default stream
+                rate of 100 kS/s, this corresponds to 163.84 ms of data. If we
+                want to stream for longer than that, we need to repeatedly write
+                half a buffer's worth of data each time the buffer is half full.
         '''
-        buffer_size = 2**14
-        if max_samples is None:
-            max_samples = int(buffer_size/2)-1
+        for channel in channels:
+            self.aScanList.extend(ljm.namesToAddresses(1, [channel])[0])
+
+        self.prepare_stream()
+        self.prepare_stream_trigger(trigger)
+
+        if trigger is not None:
+            self.stream_mode = 'out-triggered'
+        else:
+            self.stream_mode = 'out'
+
+    ''' Stream convenience functions '''
+    def get_scanrate(self, duration, max_samples):
         if self.deviceType == ljm.constants.dtT7:
-            max_speed = 100000 / channels
+            max_speed = 100000
         elif self.deviceType == ljm.constants.dtT4:
-            max_speed = 40000 / channels
+            max_speed = 40000
 
         cutoff = max_samples / max_speed
-        if period >= cutoff:
+        if duration >= cutoff:
             samples = max_samples
-            speed = samples/period
+            scanRate = int(samples/duration)
         else:
-            speed = max_speed
-            samples = period*speed
+            scanRate = max_speed
+            samples = int(duration*scanRate)
 
-        # stream = np.zeros(int(samples))
-        stream = np.zeros((int(samples), sequence.shape[1]))
-        for i in range(sequence.shape[0]):
-            for j in range(sequence.shape[1]):
-                point = sequence[i,j]
-                t = point[0]
-                V = point[1]
-                stream[int(t/period*samples)::, j] = V
-
-
-        # for point in sequence:
-        #     t = point[0]
-        #     V = point[1]
-        #     stream[int(t/period*samples)::] = V
-        return stream, speed
+        return scanRate, samples
 
     def resample(self, wave, period, max_samples = None, channels = 1):
         ''' Resamples a waveform with a given period into the optimal stream.
@@ -553,6 +477,37 @@ class LabJack(Thing):
             seq.append(point)
         seq = np.array(seq)
         return self.sequence2stream(seq, period, max_samples, wave.shape[1])
+
+    def sequence2stream(self, sequence, period, max_samples = None, channels = 1):
+        ''' Converts a sequence to a stream. The LabJack has two limitations:
+            a maximum stream rate of 100 kS/s and a maximum sample count of
+            2^13-1. This method computes a cutoff period based on these two
+            restrictions; above the cutoff, the speed will be lowered to accommodate
+            longer sequences with equal numbers of samples, while below the cutoff,
+            the number of samples will be lowered while the speed is kept at max.
+
+            Args:
+                sequence (list): A list of tuples describing all changes in a setpoint; e.g. [(0,0), (0.5,1)] describes a setpoint which is switched from 0 to 1 at t=0.5.
+                period (float): The total sequence duration.
+                channels (int): Number of channels which will be simultaneously streamed; the maximum sampling rate is reduced by this factor.
+
+            Returns:
+                stream (array): A list of points which will be output at the calculated sampling rate.
+                speed (float): Stream rate in samples/second.
+        '''
+        buffer_size = 2**14
+        if max_samples is None:
+            max_samples = int(buffer_size/2)-1
+        scanRate, samples = self.get_scanrate(period, max_samples)
+        stream = np.zeros((int(samples), sequence.shape[1]))
+        for i in range(sequence.shape[0]):
+            for j in range(sequence.shape[1]):
+                point = sequence[i,j]
+                t = point[0]
+                V = point[1]
+                stream[int(t/period*samples)::, j] = V
+
+        return stream, scanRate
 
 class MultiJack():
     ''' A shared interface for multiple LabJacks. All i/o methods use a composite channel string
