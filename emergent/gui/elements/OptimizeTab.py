a8a7b9123ab3cecbd9af5e2f152ae02e11473d39
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-02-28 17:23:01 -0700

    Remove obsolete code

--- emergent/gui/elements/OptimizeTab.py
+++ emergent/gui/elements/OptimizeTab.py
@@ -9,73 +9,6 @@ import logging as log
 import numpy as np
 from emergent.gui.elements.ParameterTable import ParameterTable
 
-class CustomTable(QTableWidget, ProcessHandler):
-    def __init__(self, parent):
-        QTableWidget.__init__(self)
-        ProcessHandler.__init__(self)
-        self.parent = parent
-
-    def contextMenuEvent(self, event):
-        self.menu = QMenu(self)
-        self.action = QAction('Tune')
-        self.action.triggered.connect(lambda: self._run_thread(self.tune))
-        self.menu.addAction(self.action)
-        self.menu.popup(QCursor.pos())
-
-    def tune(self, stopped):
-        pos = self.viewport().mapFromGlobal(QCursor.pos())
-        row = self.rowAt(pos.y())
-        name = self.item(row, 0).text()
-        settings = self.parent.get_settings_from_gui()
-        cost = getattr(settings['hub'], settings['experiment_name'])
-        sampler, index = settings['hub'].attach_sampler(settings['state'], cost)
-        algorithm_name = self.parent.algorithm_box.currentText()
-        algorithm = self.parent.parent.get_algorithm(algorithm_name, self.parent)
-        algorithm.sampler = sampler
-        algorithm.parent = settings['hub']
-        run = algorithm.run
-        default_params = algorithm.params
-        min = default_params[name].min
-        max = default_params[name].max
-        values = np.linspace(min, max, 3)
-
-        c = []
-        for v in values:
-            if stopped():
-                return
-            print('Running optimization with %s=%f...'%(name, v))
-            settings['algorithm_params'][name] = v
-            algorithm.set_params(settings['algorithm_params'])
-            settings['hub'].actuate(settings['state'])
-            run(settings['state'])
-            c.append(algorithm.sampler.history['cost'].iloc[-1])
-            print('...result:', c[-1])
-
-        print(c)
-
-
-        # state = {'thingA': {'X': 0, 'Y': 0}}
-        # cost_params = {"x0": 0.3,
-        #                "noise": 0.01,
-        #                "y0": 0.6,
-        #                "sigma_y": 0.8,
-        #                "theta": 0,
-        #                "sigma_x": 0.3,
-        #                "cycles per sample": 1}
-        # cost = self.sampler.hub.cost_uncoupled
-        # algorithm = self.adam
-        # params={'learning rate':0.1, 'steps': 100, 'dither': 0.01, 'beta_1': 0.9, 'beta_2': 0.999, 'epsilon': 1e-8}
-        # pmin = 0.001
-        # pmax = 0.1
-        # steps = 10
-        # loss = []
-        # for p in np.logspace(pmin, pmax, steps):
-        #     params['dither'] = p
-        #     algorithm(state, cost, params, cost_params)
-        #     loss.append(self.sampler.history['cost'].iloc[-1])
-        #     print(loss)
-        # return loss
-
 class OptimizeLayout(QVBoxLayout, ProcessHandler):
     def __init__(self, parent):
         QVBoxLayout.__init__(self)
@@ -148,17 +81,3 @@ class OptimizeLayout(QVBoxLayout, ProcessHandler):
         log.info('Optimization complete!')
         sampler.log(sampler.start_time.replace(':','') + ' - ' + sampler.experiment.__name__ + ' - ' + sampler.algorithm.name)
         sampler.active = False
-
-    def openMenu(self, pos):
-        item = self.itemAt(pos)
-        print(item)
-        # globalPos = self.mapToGlobal(pos)
-        # menu = QMenu()
-        # actions = {}
-        # for option in item.node.options:
-        #     actions[option] = QAction(option, self)
-        #     func = item.node.options[option]
-        #     actions[option].triggered.connect(func)
-        #     menu.addAction(actions[option])
-        #
-        # selectedItem = menu.exec_(globalPos)
