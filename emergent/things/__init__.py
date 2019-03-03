48ef9a008a479a1c924bdf904e5af1a62db3ee81
Robert Fasano <robert.j.fasano@colorado.edu>
2019-03-03 02:17:14 -0700

    Move LabJack import to last to avoid unnecessary ljm import

--- emergent/things/__init__.py
+++ emergent/things/__init__.py
@@ -1,4 +1,4 @@
-from emergent.things.labjack import LabJack
 from emergent.things.netcontrols import NetControls
 from emergent.things.novatech import Novatech
 from emergent.things.agilis import Agilis
+from emergent.things.labjack import LabJack
