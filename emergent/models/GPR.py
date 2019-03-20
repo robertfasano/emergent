074a15a8fed4852f461b933e1d0e7bf01bbe1cb9
Robert Fasano <robert.j.fasano@colorado.edu>
2019-03-20 00:42:42 -0600

    Create Experiment class

--- emergent/models/GPR.py
+++ emergent/models/GPR.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, WhiteKernel
 from emergent.models.model import Model
+import pickle
 
 class GaussianProcess(Model):
     def __init__(self):
@@ -23,12 +24,28 @@ class GaussianProcess(Model):
                                             min = 0,
                                             max = 10,
                                             description = 'Amplitude of modeled white noise process')
+
         kernel = C(self.params['Amplitude'].value, (1e-3, 1e3)) * RBF(self.params['Length scale'].value, (1e-2, 1e2)) + WhiteKernel(self.params['Noise'].value)
         self.model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
+        self.extension = '.gp'
 
+    def _export(self, filename):
+        with open(filename+self.extension, 'wb') as file:
+            pickle.dump(self.model, file)
 
     def fit(self):
         self.model.fit(self.points, self.costs)
+
+    def _import(self, filename):
+        with open(filename+self.extension, 'rb') as file:
+            self.model = pickle.load(file)
+        self.params['Amplitude'].value = self.model.kernel_.get_params()['k1__k1__constant_value']
+        self.params['Length scale'].value = self.model.kernel_.get_params()['k1__k2__length_scale']
+        self.params['Noise'].value = self.model.kernel_.get_params()['k2__noise_level']
+
+        # self.append(self.model.X_train_[-1], self.model.y_train_[-1])
+        print('Imported model %s.'%filename)
+        self.imported = True
 
     def predict(self, X):
         return self.model.predict(np.atleast_2d(X), return_std = True)
--- emergent/utilities/decorators.py
+++ emergent/utilities/decorators.py
@@ -1,14 +1,53 @@
 import decorator
 import numpy as np
 import time
+import logging as log
 
 @decorator.decorator
 def thread(func, *args, **kwargs):
     new_thread = Thread(target=func, args=args, kwargs=kwargs)
     new_thread.start()
-    
+
+from abc import abstractmethod
+class Experiment():
+    def __init__(self, sampler):
+        self.sampler = sampler
+        self.hub = sampler.hub
+
+    def _run(self):
+        params = sampler.experiment_params
+        if not sampler.skip_lock_check:
+            self.hub._check_lock()
+        if 'cycles per sample' not in params:
+            params['cycles per sample'] = 1
+        for i in range(int(params['cycles per sample'])):
+            if self.sampler.trigger is not None:
+                self.sampler.trigger()
+            c = self.run(state, params)
+            results.append(c)
+
+        c = np.mean(results)
+        error = None
+        if len(results) > 1:
+            error = np.std(results)/np.sqrt(len(results))
+
+        return c, error
+
+    @abstractmethod
+    def run(self):
+        return
+
+    @abstractmethod
+    def prepare(self):
+        return
+
+    @abstractmethod
+    def analyze(self, data):
+        return data
+
 @decorator.decorator
 def experiment(func, hub, sampler, state):
+    log.warn('Experiment decorator is deprecated; please use the new Experiment class')
     results = []
     params = sampler.experiment_params
     ''' Check that all Watchdogs report lock state. If any fail, they will attempt
