18db0e03f2d25bf86e467285e26880886115e0d6
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-03-25 14:41:25 -0600

    Successfully integrate TTL and ADC driven by EMERGENT

--- emergent/artiq/sequencer.py
+++ emergent/artiq/sequencer.py
@@ -4,6 +4,9 @@ import requests
 from flask import Flask
 from flask_socketio import SocketIO, emit
 import logging
+import pandas as pd
+from artiq.coredevice.sampler import adc_mu_to_volt
+
 
 def prepare_data_sets(times, n_samples) -> TList(TList(TInt32)):
     return [[[0]*8]*n_samples[i] for i in range(len(times))]
@@ -26,10 +29,10 @@ def get_adcs() -> TList(TList(TInt32)):
 
 def post_results(samples):
     print('sending data')
-    for i in range(len(samples)):
-        for j in range(len(samples[i])):
-            samples[i][j] = [int(x) for x in samples[i][j]]
-    requests.post('http://127.0.0.1:5000/artiq/run', json={'result': samples})
+    # for i in range(len(samples)):
+    #     for j in range(len(samples[i])):
+    #         samples[i][j] = [int(x) for x in samples[i][j]]
+    requests.post('http://127.0.0.1:5000/artiq/run', json={'result': samples.to_json()})
 
 def get_ttls(ttl_channels) -> TList(TList(TInt32)):
     ''' Queries the EMERGENT API and receives a sequence of the format
@@ -138,7 +141,7 @@ class Sequencer(EnvExperiment):
         self.core.break_realtime()
         self.sampler0.init()
         self.core.break_realtime()
-        delay(100*us)
+        delay(1*ms)
 
     @kernel
     def slack(self):
@@ -165,14 +168,16 @@ class Sequencer(EnvExperiment):
             self.set_ttl_table(ttls, ttl_channels, times, ttl_table)
 
             adc_table = get_adcs(adc_channels)
-            adc_delay = 15*us
+            adc_delay = 1*ms
             N_samples = []
             for time in times:
-                N_samples.append(int(time/delay))
+                N_samples.append(int(time/adc_delay))
             self.data = prepare_data_sets(times, N_samples)
-
-            self.execute(times, ttl_table, adc_table, adc_delay,  N_samples)
-            self.core.wait_until_mu(now_mu())       # wait until hardware cursor reaches time cursor. Important!!
+            print('times:', times)
+            print('ttl_table:', ttl_table)
+            print('adc_table:', adc_table)
+            self.execute(times, ttls, ttl_channels, ttl_table, adc_table, adc_delay,  N_samples)
+            # self.core.wait_until_mu(now_mu())       # wait until hardware cursor reaches time cursor. Important!!
 
             df = pd.DataFrame(columns=range(8))
             total_time = 0
@@ -181,39 +186,69 @@ class Sequencer(EnvExperiment):
                 for j in range(len(self.data[i])):
                     d = np.atleast_2d(self.data[i][j])
                     subdf = pd.DataFrame(d, index=[t0], columns = range(8))
-                    t0 += delay
+                    t0 += adc_delay
                     df = df.append(subdf)
 
                 total_time += times[i]
             df = adc_mu_to_volt(df)
+            print('converted')
             post_results(df)
 
     @kernel
-    def execute(self, times, ttl_table, adc_table, adc_delay, N_samples):
-        h_table = self.core_dma.get_handle("table")
+    def execute(self, times, ttls, ttl_channels, ttl_table, adc_table, adc_delay, N_samples):
         self.core.break_realtime()
-        adc_delay = 15*us
-        with parallel:
-            self.adc(times, adc_table, adc_delay, N_samples)
-            self.TTL_playback(h_table)
+        delay(50*ms)            # adjust as needed to avoid RTIO underflows
+
+        col = 0
+        for time in times:
+            start_mu = now_mu()
+            with parallel:
+                ''' TTL '''
+                with sequential:
+                    row=0
+                    for ch in ttl_channels:
+                        at_mu(start_mu)
+                        if ttl_table[row][col]==1:
+                            # ttls[ch].pulse(time)
+                            ttls[ch].on()
+                            delay(time)
+                        else:
+                            ttls[ch].off()
+                            delay(time)
+                        row = row + 1
+
+                ''' ADC '''
+                with sequential:
+                    if 1 in adc_table[col]:
+                        self.get_samples(col, N_samples[col], adc_delay)
+                    else:
+                        delay(time)
+            col += 1
 
     @kernel
-    def adc(self, times, table, delay, N_samples):
-        self.core.break_realtime()
+    def adc(self, times, table, adc_delay, N_samples):
+        delay(1*ms)
         step_number = 0
         for time in times:
             n_samples = N_samples[step_number]
             if 1 in table[step_number]:
-                self.get_samples(step_number, n_samples, delay)
+                self.get_samples(step_number, n_samples, adc_delay)
+            else:
+                delay(time)
+                print('delay')
             step_number += 1
-        return self.slack()
+        # return self.slack()
 
     @kernel
     def get_samples(self, step_number, n, dt):
+        # print('get step', step_number, 'then wait', dt, 'for', n)
         for i in range(n):
             with parallel:
-                self.sampler0.sample_mu(self.data[step_number][i])
+                with sequential:
+                    self.sampler0.sample_mu(self.data[step_number][i])
                 delay(dt)
+            # print(self.data[0])
+
 
     @kernel
     def execute_ttl_pattern(self, ttls, channels, times, table):
@@ -225,8 +260,11 @@ class Sequencer(EnvExperiment):
                 for ch in channels:
                     at_mu(start_mu)
                     if table[row][col]==1:
-                        ttls[ch].pulse(time)
+                        # ttls[ch].pulse(time)
+                        ttls[ch].on()
+                        delay(time)
                     else:
+                        ttls[ch].off()
                         delay(time)
                     row = row + 1
                 col = col + 1
@@ -239,4 +277,4 @@ class Sequencer(EnvExperiment):
     @kernel
     def TTL_playback(self, table):
         self.core_dma.playback_handle(table)
-        self.core_dma.erase("table")
+        # self.core_dma.erase("table")
