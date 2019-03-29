b0bc207a9f93eae733cb505f1f8d4091531873b6
Robert Fasano <robert.j.fasano@colorado.edu>
2019-03-29 00:51:38 -0600

    Split comms.py into dweet and slack scripts

--- emergent/protocols/dweet.py
+++ emergent/protocols/dweet.py
@@ -1,22 +1,12 @@
 import json
 import requests
 import time
-    
-class Slack():
-    def __init__(self, webhook):
-        ''' A class for relaying messages between the code and Slack. Pass in a channel's webhook at initialization. '''
-        self.webhook = webhook
-        
-    def send(self, message):
-        ''' Sends a specified message to the webhook '''
-        payload={"text": "%s"%message}
-        requests.post(self.webhook, json=payload)
-    
+
 class Dweet():
     ''' A class for remote communication through the Dweet protocol. Pass in a unique guid at initialization. '''
     def __init__(self, guid):
         self.guid = guid
-        
+
     def receive(self):
         try:
             url = 'https://dweet.io/get/latest/dweet/for/%s'%self.guid
@@ -32,8 +22,6 @@ class Dweet():
             else:
                 print(r)
 
-        
-    
     def send(self, params, vals):
         ''' Sends a list of values for named parameters to a Dweet guid '''
         url = 'https://dweet.io/dweet/for/%s?'%self.guid
@@ -41,11 +29,9 @@ class Dweet():
             url += '%s=%s&'%(params[i], vals[i])
         url = url[0:-1]
         r = json.loads(requests.get(url).text)
-        
-        return r
-   
 
-    
+        return r
+
 if __name__ == '__main__':
     thing = Dweet('YbRemoteMonitor')
     r = thing.receive()
