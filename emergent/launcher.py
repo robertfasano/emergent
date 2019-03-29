8f4cc0a55c2e7b95e5051b5c800df5abcec652e5
Robert Fasano <robert.j.fasano@colorado.edu>
2019-03-29 00:32:40 -0600

    Delete obsolete files

--- emergent/launcher.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QStyleFactory
-from emergent.gui.elements import Launcher
-import sys
-
-if __name__ == "__main__":
-    QApplication.setStyle(QStyleFactory.create("Fusion"))
-    app = QApplication(sys.argv)         # Create an instance of the application
-    launcher = Launcher(app)
-    app.exec_()
--- templates/network.py
+++
@@ -1,7 +0,0 @@
-from emergent.templates.hub import TemplateHub
-from emergent.templates.thing import TemplateThing
-from __main__ import *
-
-''' Define autoAlign '''
-hub = TemplateHub('hub', path='networks/%s'%sys.argv[1])
-thing = TemplateThing('thing', parent=hub)
