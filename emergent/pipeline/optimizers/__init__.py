361f920bca9a364388223245ca995e7329854988
Robert Fasano <robert.j.fasano@colorado.edu>
2019-03-30 11:38:40 -0600

    Add L-BFGS-B optimizer

--- emergent/pipeline/optimizers/__init__.py
+++ emergent/pipeline/optimizers/__init__.py
@@ -3,3 +3,4 @@ from emergent.pipeline.optimizers.gradient_descent import GradientDescent
 from emergent.pipeline.optimizers.grid_search import GridSearch
 from emergent.pipeline.optimizers.particle_swarm import ParticleSwarm
 from emergent.pipeline.optimizers.adam import Adam
+from emergent.pipeline.optimizers.l_bfgs_b import LBFGSB
---
+++ emergent/pipeline/optimizers/l_bfgs_b.py
@@ -0,0 +1,45 @@
+from emergent.utilities.containers import Parameter
+import numpy as np
+from scipy.optimize import minimize
+import itertools
+from emergent.pipeline import Block
+
+class LBFGSB(Block):
+    def __init__(self, params={}):
+        super().__init__()
+        self.params = {}
+
+        self.params['Tolerance'] = Parameter(name= 'Tolerance',
+                                            value = 0.01,
+                                            description = 'Convergence criterion')
+        for p in params:
+            self.params[p].value = params[p]
+
+    def measure(self, point):
+        ''' Intermediate cost function only used to store the points and costs
+            obtained by the differential evolution routine. '''
+        if self.measured_points is None:
+            self.measured_points = np.atleast_2d(point)
+        else:
+            self.measured_points = np.append(self.measured_points, np.atleast_2d(point), axis=0)
+        c = self.source.measure(point)
+        self.measured_costs = np.append(self.measured_costs, c)
+
+        return c
+
+    def run(self, points, costs, bounds=None):
+        ''' L-BFGS-B algorithm from scipy.optimize. '''
+        if bounds is None:
+            bounds = np.array(list(itertools.repeat([0, 1], points.shape[1])))
+
+        self.measured_points = None
+        self.measured_costs = np.array([])
+        res = minimize(fun=self.measure,
+                   x0=points[-1],
+                   bounds=bounds,
+                   method='L-BFGS-B',
+                   tol = self.params['Tolerance'].value)
+
+        points = np.append(points, self.measured_points, axis=0)
+        costs = np.append(costs, self.measured_costs, axis=0)
+        return points, costs
