0efab3176fdd53f522abae07f23889fdee3a6adf
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-03-29 19:47:11 -0600

    Begin ARTIQ integration

--- emergent/networks/gmot/hubs/mot.py
+++ emergent/networks/gmot/hubs/mot.py
@@ -5,9 +5,12 @@ from scipy.stats import linregress
 from scipy.optimize import curve_fit
 import numpy as np
 from emergent.modules.parallel import ProcessHandler
-from emergent.things.labjack import LabJack, LabJackSwitch
+from emergent.things.labjack import LabJack
 import matplotlib.pyplot as plt
-from emergent.utilities.testing import Timer
+import requests
+import pandas as pd
+import json
+import pickle
 # from emergent.modules.sequencing import Sequencer
 from emergent.artiq.emergent_sequencer import Sequencer
 
@@ -16,63 +19,54 @@ class MOT(Hub):
         super().__init__(name, parent = parent, network = network)
         self.process_manager = ProcessHandler()
         self.labjack = LabJack(params = {'devid': '470017907'}, name='labjack')
-        self.labjack.prepare_stream()
-        self.labjack.prepare_streamburst(channel=0)
+
+        # self.labjack.prepare_stream()
+        # self.labjack.prepare_streamburst(channel=0)
         self.labjack.AOut(0, .4)
         self.labjack.AOut(3,-5,TDAC=True)
         self.labjack.AOut(2,5, TDAC=True)
-        self.TTL = LabJack(params = {'devid': '470016970'}, name = 'TTL')
-        self.trigger_labjack = LabJack(params = {'devid': '440010734'}, name='trigger')
-        self.timer = Timer()
-
+        # self.TTL = LabJack(params = {'devid': '470016970'}, name = 'TTL')
+        # self.trigger_labjack = LabJack(params = {'devid': '440010734'}, name='trigger')
         ''' Declare TTLs '''
 
-        self.switches['trap rf'] = LabJackSwitch('trap rf', {'labjack': self.TTL, 'channel': 2}, invert = True)
-        self.switches['trap servo'] = LabJackSwitch('trap servo', {'labjack': self.TTL, 'channel': 4}, invert = True)
-        self.switches['trap shutter'] = LabJackSwitch('trap shutter', {'labjack': self.TTL, 'channel': 3})
-        self.switches['SHG rf'] = LabJackSwitch('SHG rf', {'labjack': self.TTL, 'channel': 5})
-        self.switches['SHG shutter'] = LabJackSwitch('SHG shutter', {'labjack': self.TTL, 'channel': 6})
+        # self.switches['slowing rf'] = LabJackSwitch('slowing rf', {'labjack': self.TTL, 'channel': 1}, invert = True)         # artiq 0
+        # self.switches['trap rf'] = LabJackSwitch('trap rf', {'labjack': self.TTL, 'channel': 2}, invert = True)               # 1
+        # self.switches['trap shutter'] = LabJackSwitch('trap shutter', {'labjack': self.TTL, 'channel': 3})                    # 2
 
-        self.switches['slowing shutter'] = LabJackSwitch('slowing shutter', {'labjack': self.TTL, 'channel': 7})
-        self.switches['slowing rf'] = LabJackSwitch('slowing rf', {'labjack': self.TTL, 'channel': 1}, invert = True)
-
-        steps = [{'name': 'loading', 'state': {'trap rf': 1, 'trap servo': 1, 'trap shutter': 1, 'SHG rf': 1, 'SHG shutter': 1, 'slowing shutter': 1, 'slowing rf': 1}},
-                 {'name': 'delay', 'state': {'trap rf': 1, 'trap servo': 1, 'trap shutter': 1, 'SHG rf': 0, 'SHG shutter': 1, 'slowing shutter': 0, 'slowing rf': 0}},
-                 {'name': 'probe', 'state': {'trap rf': 0, 'trap servo': 0, 'trap shutter': 0, 'SHG rf': 1, 'SHG shutter': 1, 'slowing shutter': 0, 'slowing rf': 0}},
-                 {'name': 'rf off', 'state': {'trap rf': 0, 'trap servo': 0, 'trap shutter': 0, 'SHG rf': 0, 'SHG shutter': 0, 'slowing shutter': 0, 'slowing rf': 0}},
-                 {'name': 'rf on', 'state': {'trap rf': 1, 'trap servo': 0, 'trap shutter': 0, 'SHG rf': 1, 'SHG shutter': 0, 'slowing shutter': 0, 'slowing rf': 1}}]
+        # self.switches['trap servo'] = LabJackSwitch('trap servo', {'labjack': self.TTL, 'channel': 4}, invert = True)         # 3
+                                                                                                                                # 4: slowing servo
+        # self.switches['SHG rf'] = LabJackSwitch('SHG rf', {'labjack': self.TTL, 'channel': 5})                                # 5
+        # self.switches['SHG shutter'] = LabJackSwitch('SHG shutter', {'labjack': self.TTL, 'channel': 6})                      # 6
+        # self.switches['slowing shutter'] = LabJackSwitch('slowing shutter', {'labjack': self.TTL, 'channel': 7})
 
         loading_step = {'name': 'load',
             'duration': 1.5,
-            'TTL': [1, 2, 3, 4, 5, 6, 7],
+            'TTL': [2, 5, 6, 7],       # with inverts: [1, 2, 3, 4, 5, 6, 7]
             'ADC': [0],
-            'DAC': {},
+            'DAC': {0: 0},
             'DDS': {}
            }
         delay_step = {'name': 'delay',
            'duration': 20e-3,
-           'TTL': [2, 3, 4],
+           'TTL': [2],                  # with inverts: [2, 3, 4]
            'ADC': [0],
-           'DAC': {},
+           'DAC': {0: 0},
            'DDS': {}
           }
         probe_step = {'name': 'probe',
            'duration': 15e-3,
            'TTL': [5, 6],
            'ADC': [0],
-           'DAC': {},
+           'DAC': {0: 0},
            'DDS': {}
           }
 
         steps = [loading_step, delay_step, probe_step]
-        print('MOT: Preparing initial state.')
 
-        self.sequencer = Sequencer('sequencer', parent = self, params = {'labjack': self.TTL, 'sequence': steps})
-        self.sequencer.ttl = {1: 'slowing rf', 2: 'trap rf', 3: 'trap shutter', 4: 'trap servo', 5: 'SHG rf', 6: 'SHG shutter', 7: 'slowing shutter'}
+        self.sequencer = Sequencer('sequencer', parent = self, params = {'sequence': steps})
+        self.sequencer.ttl = {0: 'slowing rf', 1: 'trap rf', 2: 'trap shutter', 3: 'trap servo', 4: 'slowing servo', 5: 'SHG rf', 6: 'SHG shutter', 7: 'slowing shutter', 8: 'test', 9: 'test', 10: 'test', 11: 'test', 12: 'test', 13: 'test', 14: 'test', 15: 'test',}
         self.sequencer.adc = {0: 'PMT'}
         self.sequencer.goto('load')
-
-        self.options['Load'] = self.ready
 
         self.result_buffer = []
 
@@ -114,120 +108,199 @@ class MOT(Hub):
         c=y1/x1**m
         return c*x**m
 
-    @experiment
-    def new_loading(self, state, params = {}):
-        self.actuate(state)
 
-        ''' Initial TTL state '''
-        for switch in ['trap rf', 'SHG rf']:
-            self.switches[switch].set(1)
-        for switch in ['trap shutter', 'trap servo', 'SHG shutter']:
-            self.switches[switch].set(0)
-        signals = []
-        for i in range(2):
-            ''' Prepare sequence '''
-            for ch in ['trap rf', 'trap servo', 'trap shutter']:
-                self.sequencer.steps[1].state[ch] = i
+    def artiq(self):
+        requests.post('http://localhost:5000/artiq/run', json={})
+        print('start:', time.time())
+        self.network.artiq_client.emit('submit', self.children['sequencer'].steps)
+        while True:
+            response = requests.get('http://localhost:5000/artiq/run').json()
+            if 'result' in response:
+                requests.post('http://localhost:5000/artiq/run', json={})
+                break
+        self.children['sequencer'].current_step = self.children['sequencer'].steps[-1]['name']
 
-            self.sequencer.prepare()
-
-            if self.labjack.stream_mode is not 'in-triggered':
-                self.labjack.prepare_streamburst(0, trigger=0)
-            self.stream_complete = False
-            self.process_manager._run_thread(self.probe_trigger, args=(0,), stoppable=False)
-            data = np.array(self.labjack.streamburst(self.sequencer.cycle_time))
-            self.stream_complete = True
-
-            probe_time = self.sequencer.state['loading']/1000 + self.sequencer.state['delay']/1000 + 1e-4
-            end_probe_time = probe_time + self.sequencer.state['probe']/1000 - 1e-4
-            time_per_point = self.sequencer.cycle_time/len(data)
-            data = data[int(probe_time/time_per_point):int(end_probe_time/time_per_point)]
-            print('Max/min: %.2f, %.2f'%(np.max(data), np.min(data)))
-            signal = np.max(data) - np.min(data)
-            signals.append(signal)
-
-
-        result = signals[1]/signals[0]
-        print('Transfer: %.2f     No transfer: %.2f    Ratio: %.2f'%(signals[1], signals[0], result))
-
-        self.result_buffer.append(result)
-        if len(self.result_buffer) > 5:
-            del self.result_buffer[0]
-        print('Last 5: %.2f +/- %.2f'%(np.mean(self.result_buffer), np.std(self.result_buffer)))
-        return -result
+        data = pd.read_json(response['result'])
+        return data
 
     @experiment
-    def transfer(self, state, params = {}):
+    def fluorescence(self, state, params = {}):
         self.actuate(state)
+        data = self.artiq()
+        print(data)
 
-        ''' Initial TTL state '''
-        for switch in ['trap rf', 'SHG rf']:
-            self.switches[switch].set(1)
-        for switch in ['trap shutter', 'trap servo', 'SHG shutter']:
-            self.switches[switch].set(0)
+        return 0
 
-        ''' Prepare sequence '''
-        for ch in ['trap rf', 'trap servo', 'trap shutter']:
-            self.sequencer.steps[1].state[ch] = 1
 
-        self.sequencer.prepare()
+    # @experiment
+    # def slope(self, state, params = {}):
+    #     self.actuate(state)
+    #     print('Preparing sequence')
+    #     self.prepare()
+    #     self.sequencer.prepare()
+    #     print('Acquiring data')
+    #     data = self.acquire()
+    #     print('Parsing data')
+    #     load_time = self.sequencer.state['loading']/1000
+    #     data = self.parse_data(data, self.sequencer.cycle_time, tmin=10e-3, tmax=load_time-10e-3)
+    #     signal = np.max(data)-np.min(data)
+    #     return -signal
+    #
+    # @experiment
+    # def lifetime(self, state, params = {'t1': 6, 't2': 12, 'threshold': 0.2}):
+    #     self.actuate(state)
+    #
+    #     self.prepare()
+    #     signals = []
+    #     for ch in ['trap rf', 'trap servo', 'trap shutter']:
+    #         self.sequencer.steps[1].state[ch] = 1
+    #     for i in range(2):
+    #         print('Preparing sequence')
+    #         ''' Prepare sequence '''
+    #         if i == 0:
+    #             self.sequencer.state['delay'] = params['t1']
+    #         if i == 1:
+    #             self.sequencer.state['delay'] = params['t2']
+    #
+    #
+    #         self.sequencer.prepare()
+    #         print('Running sequence')
+    #         data = self.acquire()
+    #         time_per_point = self.sequencer.cycle_time/len(data)
+    #         print('Parsing data')
+    #         loading_data = data[10:int(self.sequencer.state['loading']/1000/time_per_point)]
+    #         loading_signal = np.max(loading_data)-np.min(loading_data)
+    #         print('Loading signal:', loading_signal)
+    #         probe_time = self.sequencer.state['loading']/1000 + self.sequencer.state['delay']/1000 + 1e-4
+    #         end_probe_time = probe_time + self.sequencer.state['probe']/1000 - 1e-4
+    #         data = data[int(probe_time/time_per_point):int(end_probe_time/time_per_point)]
+    #         print('Max/min: %.2f, %.2f'%(np.max(data), np.min(data)))
+    #         signal = np.max(data) - np.min(data)
+    #         signals.append(signal)
+    #
+    #     short = signals[0]
+    #     long = signals[1]
+    #
+    #     result = (short-long)/short
+    #     print('Short delay: %.2f     Long delay: %.2f    Percent change: %.2f'%(signals[0], signals[1], result))
+    #
+    #     self.result_buffer.append(result)
+    #     if len(self.result_buffer) > 5:
+    #         del self.result_buffer[0]
+    #     print('Last 5: %.2f +/- %.2f'%(np.mean(self.result_buffer), np.std(self.result_buffer)))
+    #     if loading_signal < params['threshold']:
+    #         return 1
+    #     return result
 
-        if self.labjack.stream_mode is not 'in-triggered':
-            self.labjack.prepare_streamburst(0, trigger=0)
-        self.stream_complete = False
-        self.process_manager._run_thread(self.probe_trigger, args=(0,), stoppable=False)
-        data = np.array(self.labjack.streamburst(self.sequencer.cycle_time))
-        self.stream_complete = True
+    # def parse_data(self, data, duration, tmin = None, tmax = None):
+    #     ''' Returns a subset of the data array corresponding to times
+    #         greater than tmin and less than tmax. '''
+    #     time_per_point = duration/len(data)
+    #     min_point = int(tmin/time_per_point)
+    #     max_point = int(tmax/time_per_point)
+    #     return data[min_point:max_point]
 
-        results = np.mean(data)
+    # def acquire(self, duration = None):
+    #     if duration is None:
+    #         duration = self.sequencer.cycle_time
+    #     if self.labjack.stream_mode is not 'in-triggered':
+    #         self.labjack.prepare_streamburst(0, trigger=0)
+    #     self.stream_complete = False
+    #     self.process_manager._run_thread(self.probe_trigger, args=(0,), stoppable=False)
+    #     data = np.array(self.labjack.streamburst(duration))
+    #     self.stream_complete = True
+    #
+    #     return data
+    #
+    # @experiment
+    # def new_loading(self, state, params = {}):
+    #     self.actuate(state)
+    #
+    #     self.prepare()
+    #
+    #     ''' Run sequence '''
+    #     signals = []
+    #     for i in range(2):
+    #
+    #         for ch in ['trap rf', 'trap servo', 'trap shutter']:
+    #             self.sequencer.steps[1].state[ch] = i
+    #
+    #         self.sequencer.prepare()
+    #         data = self.acquire()
+    #
+    #         probe_time = self.sequencer.state['loading']/1000 + self.sequencer.state['delay']/1000 + 1e-4
+    #         end_probe_time = probe_time + self.sequencer.state['probe']/1000 - 1e-4
+    #         # time_per_point = self.sequencer.cycle_time/len(data)
+    #         # data = data[int(probe_time/time_per_point):int(end_probe_time/time_per_point)]
+    #         data = self.parse_data(data, self.sequencer.cycle_time, tmin=probe_time, tmax=end_probe_time)
+    #         print('Max/min: %.2f, %.2f'%(np.max(data), np.min(data)))
+    #         signal = np.max(data) - np.min(data)
+    #         signals.append(signal)
+    #
+    #     result = signals[1]/signals[0]
+    #     print('Transfer: %.2f     No transfer: %.2f    Ratio: %.2f'%(signals[1], signals[0], result))
+    #
+    #     self.result_buffer.append(result)
+    #     if len(self.result_buffer) > 5:
+    #         del self.result_buffer[0]
+    #     print('Last 5: %.2f +/- %.2f'%(np.mean(self.result_buffer), np.std(self.result_buffer)))
+    #     return -result
 
-        return -results
+    # def prepare(self):
+    #     ''' Initial TTL state '''
+    #     for switch in ['trap rf', 'SHG rf']:
+    #         self.switches[switch].set(1)
+    #     for switch in ['trap shutter', 'trap servo', 'SHG shutter']:
+    #         self.switches[switch].set(0)
+    #
+    # @experiment
+    # def transfer(self, state, params = {}):
+    #     self.actuate(state)
+    #     self.prepare()
+    #
+    #     ''' Prepare sequence '''
+    #     for ch in ['trap rf', 'trap servo', 'trap shutter']:
+    #         self.sequencer.steps[1].state[ch] = 1
+    #
+    #     self.sequencer.prepare()
+    #     data = self.acquire()
+    #     results = np.mean(data)
+    #
+    #     return -results
+    #
+    # @experiment
+    # def no_transfer(self, state, params = {}):
+    #     self.actuate(state)
+    #
+    #     self.prepare()
+    #
+    #     ''' Prepare sequence '''
+    #     for ch in ['trap rf', 'trap servo', 'trap shutter']:
+    #         self.sequencer.steps[1].state[ch] = 0
+    #
+    #     self.sequencer.prepare()
+    #     data = self.acquire()
+    #     results = np.mean(data)
+    #
+    #     return -results
+    #
+    # @experiment
+    # def fluorescence(self, state, params = {'settling time': 0.1, 'duration': 0.25}):
+    #     self.actuate(state)
+    #     time.sleep(params['settling time'])
+    #     # data = self.acquire(params['duration'])
+    #
+    #     return -self.labjack.AIn(0)
+    #     # return -np.mean(data)
+    #
+    # def probe_trigger(self, trigger_delay = 0.1):
+    #     i = 0
+    #     while not self.stream_complete:
+    #         i = i+1
+    #         time.sleep(trigger_delay)
+    #         self.trigger_labjack.DOut(4, i%2)
 
-    @experiment
-    def no_transfer(self, state, params = {}):
-        self.actuate(state)
-
-        ''' Initial TTL state '''
-        for switch in ['trap rf', 'SHG rf']:
-            self.switches[switch].set(1)
-        for switch in ['trap shutter', 'trap servo', 'SHG shutter']:
-            self.switches[switch].set(0)
-
-        ''' Prepare sequence '''
-        for ch in ['trap rf', 'trap servo', 'trap shutter']:
-            self.sequencer.steps[1].state[ch] = 0
-
-        self.sequencer.prepare()
-
-        if self.labjack.stream_mode is not 'in-triggered':
-            self.labjack.prepare_streamburst(0, trigger=0)
-        self.stream_complete = False
-        self.process_manager._run_thread(self.probe_trigger, args=(0,), stoppable=False)
-        data = np.array(self.labjack.streamburst(self.sequencer.cycle_time))
-        self.stream_complete = True
-
-        results = np.mean(data)
-
-        return -results
-
-    @experiment
-    def fluorescence(self, state, params = {'settling time': 0.1, 'duration': 0.25}):
-        self.actuate(state)
-        time.sleep(params['settling time'])
-        # data = self.labjack.streamburst(duration=params['duration'], operation = None)
-        # print(str(np.mean(data)*1000) + '+/-' + str(np.std(data)*1000))
-
-        return -self.labjack.AIn(0)
-        # return -np.mean(data)
-
-    def probe_trigger(self, trigger_delay = 0.1):
-        i = 0
-        while not self.stream_complete:
-            i = i+1
-            time.sleep(trigger_delay)
-            self.trigger_labjack.DOut(4, i%2)
-
-    def ready(self):
-        # self.TTL.DIO_STATE([self.PROBE_LIGHT, self.trap rf, self.trap shutter, self.trap servo, self.SHG rf, self.SHG shutter], [1, 0, 1, 0, 1, 1])
-        for switch in ['trap rf', 'trap shutter', 'trap servo', 'SHG rf', 'SHG shutter']:
-            self.switches[switch].set(1)
+    # def ready(self):
+    #     # self.TTL.DIO_STATE([self.PROBE_LIGHT, self.trap rf, self.trap shutter, self.trap servo, self.SHG rf, self.SHG shutter], [1, 0, 1, 0, 1, 1])
+    #     for switch in ['trap rf', 'trap shutter', 'trap servo', 'SHG rf', 'SHG shutter']:
+    #         self.switches[switch].set(1)
