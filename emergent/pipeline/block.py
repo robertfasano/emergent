541bb689d095f6a7d96115aa4a145f323984cfd0
Robert Fasano <robert.j.fasano@colorado.edu>
2019-03-30 16:40:16 -0600

    Remove unnecessary code

--- emergent/pipeline/block.py
+++ emergent/pipeline/block.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import time
 
 class Block():
     def __init__(self):
@@ -6,20 +7,12 @@ class Block():
 
     def connect(self, pipeline):
         self.pipeline = pipeline
+        self.number = len(pipeline.blocks)
         self.source = pipeline.source
 
     def _run(self, points, costs, bounds=None):
-        start_index = len(points)
+        self.start_index = len(points)
         points, costs = self.run(points, costs, bounds)
-        self.history_points = points[start_index::]
-        self.history_costs = costs[start_index::]
+        self.end_index = len(points)
 
         return points, costs
-
-    def plot(self):
-        if len(self.history_points) == 0:
-            return
-        plt.plot(self.history_costs, '.')
-        plt.xlabel('Evalutions')
-        plt.ylabel('Result')
-        plt.show()
