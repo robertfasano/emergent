e3564dab3570db135c74d8e28a69a5fc999687a1
Robert Fasano <robert.j.fasano@colorado.edu>
2019-04-06 17:24:54 -0600

    Add Loop block

--- emergent/pipeline/blocks/__init__.py
+++ emergent/pipeline/blocks/__init__.py
@@ -1,3 +1,4 @@
 from emergent.pipeline.blocks.rescale import Rescale
 from emergent.pipeline.blocks.prune import Prune
-from emergent.pipeline.blocks.fit import Fit 
+from emergent.pipeline.blocks.fit import Fit
+from emergent.pipeline.blocks.loop import Loop
---
+++ emergent/pipeline/blocks/loop.py
@@ -0,0 +1,25 @@
+from emergent.pipeline import BasePipeline
+from emergent.utilities.containers import Parameter
+
+class Loop(BasePipeline):
+    def __init__(self, params = {'Loops': 1}):
+        super().__init__()
+        self.params = {}
+        self.params['Loops'] = Parameter(name='Loops',
+                                         value=1,
+                                         type=int)
+        for p in params:
+            self.params[p].value = params[p]
+
+    def measure(self, point):
+        return self.pipeline.measure(point)
+
+    def run(self, points, costs, bounds=None):
+        self.bounds = self.pipeline.bounds
+        self.points = points.copy()
+        self.costs = costs.copy()
+        for i in range(self.params['Loops'].value):
+            for block in self.blocks:
+                self.points, self.costs = block.run(self.points, self.costs, self.bounds)
+                self.pipeline.bounds = self.bounds # update parent bounds in case they changed
+        return self.points, self.costs
