48d3e39f295a1370d32fdfab3d5b16ce53cf1d77
Robert Fasano <robert.j.fasano@colorado.edu>
2019-04-06 15:55:44 -0600

    Add 1D plot method

--- emergent/pipeline/models/model.py
+++ emergent/pipeline/models/model.py
@@ -1,6 +1,9 @@
 from abc import abstractmethod
 from emergent.pipeline import BasePipeline
 import numpy as np
+import pyqtgraph as pg
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
 
 class Model(BasePipeline):
     ''' The Model class inherits elements from both Pipeline and Block - it is a subpipeline
@@ -43,3 +46,35 @@ class Model(BasePipeline):
         points = np.append(points, np.atleast_2d(self.best_point), axis=0)
         costs = np.append(costs, best_cost)
         return points, costs
+
+    def plot(self, axis):
+        ''' Plots a 1D cross-section through the minimum of the modeled surface. '''
+        dim = self.best_point.shape[0]
+        n_points = 100
+
+        points = np.zeros((n_points, dim))
+        for d in range(dim):
+            points[:, d] = self.best_point[d]
+        points[:, axis] = np.linspace(self.pipeline.bounds[axis][0],
+                                      self.pipeline.bounds[axis][1],
+                                      n_points)
+        costs = self.predict(points)[0]
+        win = pg.GraphicsWindow()
+        win.setWindowTitle('Modeled surface')
+        label = pg.LabelItem(justify = "right")
+        label.setText('goo!')
+        win.addItem(label)
+        p = win.addPlot(row = 1, col = 0, labels={'bottom': 'Axis %i'%axis, 'left': 'Result'})
+
+        def mouseMoved(evt):
+            mousePoint = p.vb.mapSceneToView(evt[0])
+            label.setText("x = %0.2f, y = %0.2f</span>" % (mousePoint.x(), mousePoint.y()))
+
+        points = self.pipeline.unnormalize(points)
+        x = points[:,axis]
+        y = costs
+        plot = p.plot(x=x, y=y, symbol='o', symbolSize=7, pen=None)
+
+        proxy = pg.SignalProxy(p.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
+
+        return win, proxy
