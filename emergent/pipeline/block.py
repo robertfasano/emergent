e2a14225ae788e9c3d4d89e3e70f804f402a087d
Robert Fasano <robert.j.fasano@colorado.edu>
2019-04-16 22:07:20 -0400

    Add hyperparameter tuning

--- emergent/pipeline/block.py
+++ emergent/pipeline/block.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import time
+import numpy as np
 
 class Block():
     def __init__(self):
@@ -11,3 +12,43 @@ class Block():
         self.end_index = len(points)
 
         return points, costs
+
+    def tune(self, parameter, bounds, steps=20, mode='improvement'):
+        ''' Args:
+                parameter (str): the parameter to optimize
+                bounds (tuple): optimization range
+                mode (str): 'iterations': optimize the number of iterations for convergence
+                            'result': optimize the final result
+                            'improvement': optimize the improvement per iteration
+        '''
+        from emergent.pipeline import Pipeline
+
+        meta_points = np.logspace(np.log10(bounds[0]),
+                                  np.log10(bounds[1]),
+                                  steps)
+        meta_costs = []
+        for point in meta_points:
+            pipe = Pipeline(self.pipeline.experiment,
+                            self.pipeline.params,
+                            self.pipeline.state,
+                            self.pipeline.scaler.limits,
+                            verbose=False)
+            self.params[parameter].value = point
+            pipe.add(self)
+            points, costs = pipe.run()
+            if mode == 'iterations':
+                meta_costs.append(len(points))
+            elif mode == 'improvement':
+                meta_costs.append(-(costs[-1]-costs[0])/len(points))
+            elif mode == 'result':
+                meta_costs.append(-(costs[-1]-costs[0]))
+
+        plt.plot(meta_points, meta_costs)
+        plt.xlabel(parameter)
+        labels = {'iterations': 'Iterations for convergence',
+                  'improvement': 'Improvement per iteration',
+                  'result': 'Final result'}
+        plt.ylabel(labels[mode])
+        if bounds[1]/bounds[0] > 100:
+            plt.xscale('log')
+        plt.show()
