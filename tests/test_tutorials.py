df8d04facfbe380d7e70951b3ff5e174a6671b42
Robert Fasano <robert.j.fasano@colorado.edu>
2019-04-18 01:43:23 -0400

    Add new official modules docs to autotesting

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -3,7 +3,7 @@ import os
 from nbconvert.preprocessors import ExecutePreprocessor
 
 os.chdir('../tutorials')
-notebooks = ['Getting started', 'Modeling and optimization', 'HTTP API', 'Pipelines', 'Monitoring']
+notebooks = ['Getting started', 'HTTP API', 'Monitoring', 'modules/sequencing', 'modules/core', 'modules/optimize']
 for notebook in notebooks:
     print('Testing %s notebook.'%notebook)
     notebook_filename = '%s.ipynb'%notebook
