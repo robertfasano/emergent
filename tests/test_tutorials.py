eac159dc1e00a2845c4c6a25c9b2e2980a4a23e2
Robert Fasano <robert.j.fasano@colorado.edu>
2019-04-20 11:11:56 -0600

    Update testing based on new tutorials

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -2,16 +2,19 @@ import nbformat
 import os
 from nbconvert.preprocessors import ExecutePreprocessor
 
-os.chdir('../tutorials')
-notebooks = ['Getting started', 'HTTP API', 'Monitoring', 'modules/sequencing', 'modules/core', 'modules/optimize']
+os.chdir('../tutorials/modules')
+notebooks = ['monitor', 'sequencing', 'core', 'optimize']
 for notebook in notebooks:
     print('Testing %s notebook.'%notebook)
     notebook_filename = '%s.ipynb'%notebook
 
     with open(notebook_filename) as f:
         nb = nbformat.read(f, as_version=4)
+    print('\tSuccessfully read notebook...')
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
     ep.preprocess(nb, {})
+    print('\tSuccessfully executed notebook...')
+
     with open('%s_executed.ipynb'%notebook, 'wt') as f:
         nbformat.write(nb, f)
     assert '%s_executed.ipynb'%notebook in os.listdir()
