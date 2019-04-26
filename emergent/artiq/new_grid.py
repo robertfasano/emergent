68ffa817e42a9e85264a4b2bb5f6d89d7ca9ccce
Robert Fasano <robert.fasano@nist.gov>
2019-04-26 17:18:25 -0600

    Allow editing of timestep names; autocalculate cycle time when step durations are changed; allow dynamic step addition through the GUI

--- emergent/artiq/new_grid.py
+++ emergent/artiq/new_grid.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtWidgets import (QApplication, QLabel, QLineEdit, QMenu, QAction, QPushButton,
+from PyQt5.QtWidgets import (QApplication, QLabel, QLineEdit, QMenu, QAction, QPushButton, QStackedWidget,
         QWidget, QInputDialog, QToolButton, QCheckBox, QHBoxLayout, QVBoxLayout, QGridLayout, QSizePolicy, QComboBox)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter
@@ -8,6 +8,32 @@ from emergent.dashboard.structures.icon_button import IconButton
 import time
 import numpy as np
 # from emergent.artiq.sequencer_table import SequencerTable
+
+class EditableLabel(QWidget):
+    def __init__(self, text):
+        super().__init__()
+        self.label = QLabel(text)
+        self.edit = QLineEdit(text)
+        self.edit.returnPressed.connect(self.returnEvent)
+        self.setLayout(QVBoxLayout())
+
+        self.stack = QStackedWidget()
+        self.stack.addWidget(self.label)
+        self.stack.addWidget(self.edit)
+        self.layout().addWidget(self.stack)
+
+    def mouseDoubleClickEvent(self, event):
+        self.stack.setCurrentIndex(1-self.stack.currentIndex())
+
+    def returnEvent(self):
+        self.label.setText(self.edit.text())
+        self.stack.setCurrentIndex(1-self.stack.currentIndex())
+
+    def setBold(self, bold):
+        if bold:
+            self.label.setText('<b>'+self.label.text()+'</b>')
+        else:
+            self.label.setText(self.label.text())
 
 class ttlLabel(QLabel):
     def __init__(self, name, channel, grid):
@@ -113,6 +139,7 @@ class StepEdit(QLineEdit):
         self.setText(str(value))
         # state = {'sequencer':{self.name: value}}
         self.grid.push_sequence()
+        self.grid.update_cycle_time()
 
 class BoldLabel(QLabel):
     def __init__(self, name):
@@ -125,26 +152,31 @@ class BoldLabel(QLabel):
         else:
             self.setText(self.name)
 
-class StepLabel(BoldLabel):
-    def __init__(self, name, grid):
+class StepLabel(EditableLabel):
+    def __init__(self, name, grid, index):
         super().__init__(name)
+        self.index = index
+        self.name = name
         self.grid = grid
         self.customContextMenuRequested.connect(self.openMenu)
         self.setContextMenuPolicy(Qt.CustomContextMenu)
 
+        self.edit.returnPressed.connect(self.update_name)
+
     def openMenu(self, pos):
         globalPos = self.mapToGlobal(pos)
         menu = QMenu()
-        move_menu = menu.addMenu('Move')
-
-        move_options = {'Left': self.move_left,
-                   'Right': self.move_right }
+        # move_menu = menu.addMenu('Move')
+        #
+        # move_options = {'Left': self.move_left,
+        #            'Right': self.move_right }
         actions = {}
 
-        for option in move_options:
-            actions[option] = QAction(option, self)
-            actions[option].triggered.connect(move_options[option])
-            move_menu.addAction(actions[option])
+        # for option in move_options:
+        #     actions[option] = QAction(option, self)
+        #     actions[option].triggered.connect(move_options[option])
+        #     move_menu.addAction(actions[option])
+
         selectedItem = menu.exec_(globalPos)
 
     def move_left(self):
@@ -152,6 +184,11 @@ class StepLabel(BoldLabel):
 
     def move_right(self):
         self.grid.move(self.name, 1)
+
+    def update_name(self):
+        self.name = self.edit.text()
+        self.grid.push_sequence()
+
 
 
 
@@ -165,7 +202,7 @@ class GridWindow(QWidget):
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding);
         self.dashboard = dashboard
         self.picklable = False
-        self.dashboard.test_signal.connect(lambda: self.insert_step('test', 0))
+        # self.dashboard.test_signal.connect(lambda: self.insert_step('test', 0))
         # self.dashboard.test_signal.connect(lambda: self.swap_timesteps('load', 'probe'))
 
         self.layout = QVBoxLayout()
@@ -181,6 +218,9 @@ class GridWindow(QWidget):
         self.selector_layout.addWidget(self.store_button)
         self.delete_button = IconButton('dashboard/gui/media/Material/trash.svg', self.delete)
         self.selector_layout.addWidget(self.delete_button)
+        self.selector_layout.addWidget(IconButton('dashboard/gui/media/Material/content-add.svg', self.insert_step))
+
+        self.selector_layout.addWidget(IconButton('dashboard/gui/media/Material/outline-play-arrow.svg', self.play))
         self.layout.addLayout(self.selector_layout)
 
         self.widget = QWidget()
@@ -194,7 +234,7 @@ class GridWindow(QWidget):
         timesteps = self.dashboard.get('artiq/sequence')
         self.draw(timesteps)
 
-        self.dashboard.timestep_signal.connect(self.bold_active_step)
+        # self.dashboard.timestep_signal.connect(self.bold_active_step)
         self.dashboard.sequence_update_signal.connect(self.refresh)
 
     def activate(self, sequence):
@@ -276,7 +316,6 @@ class GridWindow(QWidget):
         # for dds in self.dds:
         #     self.grid_layout.addWidget(QLabel(str(dds)), row, 0)
         #     row += 1
-
     def refresh(self, sequence=None):
         # if sequence is None:
         sequence = self.dashboard.get('artiq/sequence')
@@ -284,11 +323,11 @@ class GridWindow(QWidget):
 
     def draw(self, sequence):
         self.order = []
-        self.labels = {}
-        self.step_edits = {}
-        self.checkboxes = {}
-        self.adc_checkboxes = {}
-        self.dac_edits = {}
+        self.labels = []
+        self.step_edits = []
+        self.checkboxes = []
+        self.adc_checkboxes = []
+        self.dac_edits = []
         col = 1
         row = 0
         for step in sequence:
@@ -298,40 +337,44 @@ class GridWindow(QWidget):
         self.total_time_label = QLabel('%.1f ms'%total_cycle_time)
         self.grid_layout.addWidget(QLabel('Cycle time:'), row, 0)
         self.grid_layout.addWidget(self.total_time_label, row, 1)
-        self.bold_active_step()
-
+        # self.bold_active_step()
+        QApplication.processEvents()        # determine new minimumSize
+        self.resize(self.minimumSize())
     def redraw(self, sequence):
-        for step in self.labels:
+        for step in range(len(self.labels)):
             self.remove_step(step)
         self.grid_layout.removeWidget(self.total_time_label)
         self.total_time_label.deleteLater()
         self.draw(sequence)
-        QApplication.processEvents()        # determine new minimumSize
-        self.resize(self.minimumSize())
+
+    def update_cycle_time(self):
+        total_cycle_time = self.get_cycle_time()*1000
+        self.total_time_label.setText('%.1f ms'%total_cycle_time)
 
     def get_cycle_time(self):
         T = 0
-        for edit in self.step_edits.values():
+        for edit in self.step_edits:
             T += float(edit.text())
         return T
 
     def get_sequence(self):
         sequence = []
-        for name in self.order:
+        for i in range(len(self.labels)):
+            name = self.labels[i].name
             new_step = {}
-            new_step['duration'] = float(self.step_edits[name].text())
+            new_step['duration'] = float(self.step_edits[i].text())
             new_step['name'] = name
             new_step['TTL'] = []
             new_step['ADC'] = []
             new_step['DAC'] = {}
             for ttl in self.ttls:
-                if self.checkboxes[name][ttl].isChecked():
+                if self.checkboxes[i][ttl].isChecked():
                     new_step['TTL'].append(ttl)
             for adc in self.adcs:
-                if self.adc_checkboxes[name][adc].isChecked():
+                if self.adc_checkboxes[i][adc].isChecked():
                     new_step['ADC'].append(adc)
             for dac in self.dacs:
-                new_step['DAC'][int(dac)] = float(self.dac_edits[name][dac].text())
+                new_step['DAC'][int(dac)] = float(self.dac_edits[i][dac].text())
             sequence.append(new_step)
         return sequence
 
@@ -339,25 +382,25 @@ class GridWindow(QWidget):
         name = step['name']
         ''' Add label and edit '''
         self.order.append(name)
-        self.labels[name] = StepLabel(name, self)
-        self.grid_layout.addWidget(self.labels[name], 0, col)
-        self.step_edits[name] = StepEdit(name, str(step['duration']), self.dashboard, self)
-        self.grid_layout.addWidget(self.step_edits[name], 1, col)
+        self.labels.append(StepLabel(name, self, col))
+        self.grid_layout.addWidget(self.labels[-1], 0, col)
+        self.step_edits.append(StepEdit(name, str(step['duration']), self.dashboard, self))
+        self.grid_layout.addWidget(self.step_edits[-1], 1, col)
 
         ''' Add TTL checkboxes '''
         row = 3
-        self.checkboxes[name] = {}
+        self.checkboxes.append({})
         for switch in self.ttls:
             state = 0
             if int(switch) in step['TTL'] or str(switch) in step['TTL']:
                 state = 1
             box = StateCheckbox(name, step, state, self.dashboard, self)
             self.grid_layout.addWidget(box, row, col)
-            self.checkboxes[name][switch] = box
+            self.checkboxes[-1][switch] = box
             row += 1
 
         ''' Add ADC checkboxes '''
-        self.adc_checkboxes[name] = {}
+        self.adc_checkboxes.append({})
         row += 1
         for adc in self.adcs:
             state = 0
@@ -365,11 +408,11 @@ class GridWindow(QWidget):
                 state = 1
             box = StateCheckbox(name, step, state, self.dashboard, self)
             self.grid_layout.addWidget(box, row, col)
-            self.adc_checkboxes[name][adc] = box
+            self.adc_checkboxes[-1][adc] = box
             row += 1
 
         ''' Add DAC edits '''
-        self.dac_edits[name] = {}
+        self.dac_edits.append({})
         row += 1
         for dac in self.dacs:
             if int(dac) in step['DAC']:
@@ -380,7 +423,7 @@ class GridWindow(QWidget):
                 value = 0.0
             edit = DACEdit(name, str(value), self.dashboard, self)
             self.grid_layout.addWidget(edit, row, col)
-            self.dac_edits[name][dac] = edit
+            self.dac_edits[-1][dac] = edit
             row += 1
         return row
 
@@ -395,31 +438,28 @@ class GridWindow(QWidget):
         for widget in remove:
             self.grid_layout.removeWidget(widget)
             widget.deleteLater()
+    #
+    # def bold_active_step(self, current_step=None):
+    #     steps = self.dashboard.get('artiq/sequence')
+    #     if current_step is None:
+    #         current_step = self.dashboard.get('artiq/current_step')
+    #     else:
+    #         current_step = current_step['name']
+    #     for step in steps:
+    #         if step['name'] == current_step:
+    #             self.labels[step['name']].setBold(True)
+    #         else:
+    #             self.labels[step['name']].setBold(False)
 
-    def bold_active_step(self, current_step=None):
-        steps = self.dashboard.get('artiq/sequence')
-        if current_step is None:
-            current_step = self.dashboard.get('artiq/current_step')
-        else:
-            current_step = current_step['name']
-        for step in steps:
-            if step['name'] == current_step:
-                self.labels[step['name']].setBold(True)
-            else:
-                self.labels[step['name']].setBold(False)
-
-    def insert_step(self, name, position = -1):
-        step = {'name': name,
+    def insert_step(self, name=''):
+        step = {'name': '',
                 'duration': 0,
                 'TTL': [],
                 'ADC': [],
                 'DAC': {},
                 'DDS': {}}
-        if step in self.order:
-            log.warn('Step already exists!')
-            return
         steps = self.get_sequence()
-        steps.insert(position, step)
+        steps.append(step)
         self.redraw(steps)
 
         self.dashboard.post('artiq/sequence', steps)
@@ -469,12 +509,18 @@ class GridWindow(QWidget):
             self.swap(row, col1, row, col2)
             time.sleep(1e-6)            # need a short pause here or Qt will crash
 
-    def swap_timesteps(self, name1, name2):
-        widget1 = self.labels[name1]
-        index1 = self.grid_layout.indexOf(widget1)
-        row1, col1, rowspan, colspan = self.grid_layout.getItemPosition(index1)
-        widget2 = self.labels[name2]
-        index2 = self.grid_layout.indexOf(widget2)
-        row2, col2, rowspan, colspan = self.grid_layout.getItemPosition(index2)
+    # def swap_timesteps(self, name1, name2):
+    #     widget1 = self.labels[name1]
+    #     index1 = self.grid_layout.indexOf(widget1)
+    #     row1, col1, rowspan, colspan = self.grid_layout.getItemPosition(index1)
+    #     widget2 = self.labels[name2]
+    #     index2 = self.grid_layout.indexOf(widget2)
+    #     row2, col2, rowspan, colspan = self.grid_layout.getItemPosition(index2)
+    #
+    #     self.swap_columns(col1, col2)
 
-        self.swap_columns(col1, col2)
+    def play(self):
+        print(self.get_sequence())
+
+        import os
+        os.system('start "" cmd /k "cd /emergent/emergent/artiq/ & call activate artiq-4 & artiq_run sequencer_loop.py"')
