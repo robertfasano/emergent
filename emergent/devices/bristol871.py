a76ea65d8cf3a931dfd110ea5ff648fe907ea2a9
Robert Fasano <robert.fasano@nist.gov>
2019-04-30 17:15:27 -0600

    Add sample rate knob

--- emergent/devices/bristol871.py
+++ emergent/devices/bristol871.py
@@ -1,8 +1,9 @@
-from emergent.core import Device, Sensor
+from emergent.core import Device, Sensor, Knob
 
 class Bristol871(Device):
     frequency = Sensor('frequency')
     power = Sensor('power')
+    sample_rate = Knob('sample_rate')
 
     def __init__(self, name, hub, params={'addr':'10.199.199.1', 'port': 23}):
         super().__init__(name, hub, params)
@@ -22,13 +23,17 @@ class Bristol871(Device):
 
     def _query(self, msg, threshold = None):
         self.client.sendall(b'%s\n'%msg)
-        resp = float(str(self.client.recv(1024), 'utf-8').split('\r')[0])
+        resp = str(self.client.recv(1024), 'utf-8').split('\r')[0]
 
         return resp
 
+    def _command(self, msg, threshold = None):
+        self.client.sendall(b'%s\n'%msg)
+
+
     @frequency.query
     def frequency(self):
-        f = self._query(b':READ:FREQ?')
+        f = float(self._query(b':READ:FREQ?'))
         if f < 1e-3:          # detect read failures and return None
             return None
         else:
@@ -36,4 +41,12 @@ class Bristol871(Device):
 
     @power.query
     def power(self):
-        return self._query(b':READ:POWER?')
+        return float(self._query(b':READ:POWER?'))
+
+    @sample_rate.query
+    def sample_rate(self):
+        return int(self._query(b':TRIG:SEQ:RATE?'))
+
+    @sample_rate.command
+    def sample_rate(self, new_rate):
+        return self._command(b':TRIG:SEQ:RATE %i'%new_rate)
